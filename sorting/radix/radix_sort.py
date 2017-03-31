1c50610bbc0d71b1e5b735bee6bbc3ad6f6b5a5d
Legolas Bloom <zhanhsw@gmail.com>
2017-03-31 22:40:31 +0800

    use list comprehension

--- sorting/radix/radix_sort.py
+++ sorting/radix/radix_sort.py
@@ -8,11 +8,9 @@ def radix_sort(data, radix=10):
         bucket = [[] for i in range(radix)]
         for val in data:
             bucket[val % radix**digit / radix**(digit-1)].append(val)
-        del data[:]
-        for each in bucket:
-            data.extend(each)
+        data = [val for vals in bucket for val in vals]
+    return data
 
 
 data = [123, 21, 23, 4, 54, 3214, 546, 466, 34, 1]
-radix_sort(data, radix=10)
-print data
+print radix_sort(data, radix=10)
