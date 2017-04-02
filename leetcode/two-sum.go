1efbed48635277b11d00183133709bd75c5b7119
Legolas Bloom <zhanhsw@gmail.com>
2017-04-02 21:13:40 +0800

    finished two sum

---
+++ leetcode/two-sum.go
@@ -0,0 +1,35 @@
+/* https://leetcode.com/problems/two-sum/#/description
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	length := len(nums)
+	m := make(map[int]int, length)
+	for i := 0; i < length; i++ {
+		diff := target - nums[i]
+		if j, ok := m[diff]; ok {
+			return []int{j, i}
+		}
+		m[nums[i]] = i
+	}
+	panic("no two sum solution")
+}
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	result := twoSum(nums, 9)
+	if result[0] != 0 || result[1] != 1 {
+		fmt.Println("error")
+	}
+}
