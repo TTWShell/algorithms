94a49dc33f1b30c1c7a304986bc15fb547c0a208
Legolas Bloom <zhanhsw@gmail.com>
2017-05-09 16:31:07 +0800

    add test for valid parentheses

--- leetcode/valid-parentheses_test.go
+++ leetcode/valid-parentheses_test.go
@@ -3,8 +3,8 @@ package leetcode
 import "testing"
 
 func Test_isValid(t *testing.T) {
-	inputs := []string{"[", "()", "{()}[]", "{[](}"}
-	results := []bool{false, true, true, false}
+	inputs := []string{"]()", "[", "()", "{()}[]", "{[](}"}
+	results := []bool{false, false, true, true, false}
 	for i := 0; i < len(inputs); i++ {
 		if r := isValid(inputs[i]); r != results[i] {
 			t.Log(inputs[i], results[i], r)
