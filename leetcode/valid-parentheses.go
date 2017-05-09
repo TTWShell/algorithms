2ce2eb0e00ea80ab3aec5a09dab637000f2095a5
Legolas Bloom <zhanhsw@gmail.com>
2017-05-09 16:29:43 +0800

    use []byte instead string

--- leetcode/valid-parentheses.go
+++ leetcode/valid-parentheses.go
@@ -2,31 +2,25 @@
 Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
-
-Subscribe to see which companies asked this question.
 */
 package leetcode
 
 func isValid(s string) bool {
-	var str string
+	var stack []byte
 	for i := 0; i < len(s); i++ {
-		if v := s[i]; v == '(' || v == '[' || v == '{' {
-			str += string(v)
-		} else if len(str) == 0 {
-			return false
-		} else {
-			v1 := str[len(str)-1]
-			if v == ')' && v1 != '(' {
-				return false
-			}
-			if v == ']' && v1 != '[' {
-				return false
-			}
-			if v == '}' && v1 != '{' {
-				return false
-			}
-			str = str[:len(str)-1]
+		v := s[i]
+		if v == '(' || v == '[' || v == '{' {
+			stack = append(stack, v)
+			continue
 		}
+		if len(stack) == 0 {
+			return false
+		}
+		v1 := stack[len(stack)-1]
+		if (v == ')' && v1 != '(') || (v == ']' && v1 != '[') || (v == '}' && v1 != '{') {
+			return false
+		}
+		stack = stack[:len(stack)-1]
 	}
-	return len(str) == 0
+	return len(stack) == 0
 }
