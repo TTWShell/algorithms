79513a43d68d2ea380c17de3e7571034389306f6
Legolas Bloom <zhanhsw@gmail.com>
2017-05-11 11:08:01 +0800

    add remove element

---
+++ leetcode/remove-element_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import "testing"
+
+func Test_removeElement(t *testing.T) {
+	input := []int{3, 2, 2, 4, 3, 1, 4, 5, 5, 6}
+	result := []int{2, 2, 4, 1, 4, 5, 5, 6}
+	length := removeElement(input, 3)
+	for i := 0; i < len(result); i++ {
+		if length != 8 || input[i] != result[i] {
+			t.Log(length, input[i], result[i])
+			t.Fail()
+		}
+	}
+}
