d87a81f8f46858874561cebc0efed71d8f95de14
Legolas Bloom <zhanhsw@gmail.com>
2017-05-12 16:28:12 +0800

    enhance test

--- leetcode/addBinary_test.go
+++ leetcode/addBinary_test.go
@@ -3,9 +3,9 @@ package leetcode
 import "testing"
 
 func Test_addBinary(t *testing.T) {
-	as := []string{"0", "1", "11", "11"}
-	bs := []string{"1", "1", "10", "1"}
-	rs := []string{"1", "10", "101", "100"}
+	as := []string{"0", "0", "1", "11", "11", "1111"}
+	bs := []string{"0", "1", "1", "10", "1", "1111"}
+	rs := []string{"0", "1", "10", "101", "100", "11110"}
 	for i := 0; i < len(as); i++ {
 		if r := addBinary(as[i], bs[i]); r != rs[i] {
 			t.Log(as[i], bs[i], rs[i], r)
