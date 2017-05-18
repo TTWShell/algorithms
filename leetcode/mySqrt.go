d422d3307136e938ac863ed52b146d6e9c4518f8
Legolas Bloom <zhanhsw@gmail.com>
2017-05-18 18:03:14 +0800

    add mySqrt

---
+++ leetcode/mySqrt.go
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/sqrtx/#/description
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+*/
+
+package leetcode
+
+func mySqrt(x int) int {
+	if x <= 1 {
+		return x
+	}
+	start, end := 0, x/2
+	for start <= end {
+		mid := start + (end-start)/2
+		square := mid * mid
+		if square < x {
+			start = mid + 1
+		} else if square == x {
+			return mid
+		} else {
+			end = mid - 1
+		}
+	}
+	return end
+}
---
+++ leetcode/mySqrt_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	nums := []int{0, 1, 2, 4, 6, 10, 8192}
+	results := []int{0, 1, 1, 2, 2, 3, 90}
+	for i := 0; i < len(nums); i++ {
+		if result := mySqrt(nums[i]); result != results[i] {
+			t.Log(nums[i], results[i], result)
+			t.Fail()
+		}
+	}
+}
