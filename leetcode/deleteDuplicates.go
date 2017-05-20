a7be707ba439fec694d79b3c5f91d3f68dcc252b
Legolas Bloom <zhanhsw@gmail.com>
2017-05-20 14:44:29 +0800

    deleteDuplicates

---
+++ leetcode/deleteDuplicates.go
@@ -0,0 +1,35 @@
+/* https://leetcode.com/problems/remove-duplicates-from-sorted-list/#/description
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+For example,
+Given 1->1->2, return 1->2.
+Given 1->1->2->3->3, return 1->2->3.
+*/
+
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	var r, c, temp *ListNode
+	if head != nil {
+		c = &ListNode{Val: head.Val}
+		r = c
+		head = head.Next
+	}
+
+	for head != nil {
+		if c.Val != head.Val {
+			temp = &ListNode{Val: head.Val}
+			c.Next = temp
+			c = temp
+		}
+		head = head.Next
+	}
+	return r
+}
---
+++ leetcode/deleteDuplicates_test.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import "testing"
+
+func Test_deleteDuplicates(t *testing.T) {
+	input := &ListNode{Val: 1, Next: &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 3, Next: nil}}}}}
+	if result := deleteDuplicates(input); result.String() != "1 2 3 <nil>" {
+		t.Log(result, "1 2 3 <nil>")
+		t.Fail()
+	}
+}
