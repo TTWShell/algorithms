9da40f0723a07386640a00198926364843ac8cff
Legolas Bloom <zhanhsw@gmail.com>
2017-05-21 11:36:40 +0800

    add isSymmetric

---
+++ leetcode/isSymmetric.go
@@ -0,0 +1,45 @@
+/* https://leetcode.com/problems/symmetric-tree/#/description
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+*/
+
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isMirror(left, right *TreeNode) bool {
+	return (left == nil && right == nil) ||
+		(left != nil && right != nil &&
+			left.Val == right.Val &&
+			isMirror(left.Left, right.Right) &&
+			isMirror(right.Left, left.Right))
+}
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
---
+++ leetcode/isSymmetric_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import "testing"
+
+func Test_isSymmetric(t *testing.T) {
+	root := &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 3}, Right: &TreeNode{Val: 4}}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 3}}}
+	if r := isSymmetric(root); r != true {
+		t.Log(r)
+		t.Fail()
+	}
+
+	root = &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}}
+	if r := isSymmetric(root); r != false {
+		t.Log(r)
+		t.Fail()
+	}
+}
