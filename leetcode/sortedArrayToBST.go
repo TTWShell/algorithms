9de34679def1f0e39c37126084d1e86a3b4e1230
Legolas Bloom <zhanhsw@gmail.com>
2017-05-22 15:24:03 +0800

    report cover when run test

---
+++ leetcode/sortedArrayToBST.go
@@ -0,0 +1,17 @@
+/* https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/#/description
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+*/
+
+package leetcode
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	return nil
+}
---
+++ leetcode/sortedArrayToBST_test.go
@@ -0,0 +1,7 @@
+package leetcode
+
+import "testing"
+
+func Test_sortedArrayToBST(t *testing.T) {
+
+}
