24b35f105b25605c10d0aae335ad0d2a6ffd4a98
Legolas Bloom <zhanhsw@gmail.com>
2017-05-22 21:58:44 +0800

    add sortedArrayToBST

--- leetcode/sortedArrayToBST.go
+++ leetcode/sortedArrayToBST.go
@@ -13,5 +13,17 @@ package leetcode
  * }
  */
 func sortedArrayToBST(nums []int) *TreeNode {
-	return nil
+	return toBST(nums, 0, len(nums)-1)
+}
+
+func toBST(nums []int, start, end int) *TreeNode {
+	if start > end {
+		return nil
+	}
+
+	mid := start + (end-start)/2
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = toBST(nums, start, mid-1)
+	root.Right = toBST(nums, mid+1, end)
+	return root
 }
--- leetcode/sortedArrayToBST_test.go
+++ leetcode/sortedArrayToBST_test.go
@@ -3,5 +3,9 @@ package leetcode
 import "testing"
 
 func Test_sortedArrayToBST(t *testing.T) {
-
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	if r := sortedArrayToBST(nums); r.String() != "<<<<nil> 1 <nil>> 2 <<nil> 3 <nil>>> 4 <<<nil> 5 <nil>> 6 <<nil> 7 <nil>>>>" {
+		t.Log(nums, r.String())
+		t.Fail()
+	}
 }
