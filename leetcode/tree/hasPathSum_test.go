22d475e198629c0bab370d0c1aee2bb7f95e2924
Legolas Bloom <zhanhsw@gmail.com>
2017-05-23 17:45:35 +0800

    add hasPathSum

---
+++ leetcode/tree/hasPathSum_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import "testing"
+
+func Test_hasPathSum(t *testing.T) {
+	root := &TreeNode{Val: 5, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 8, Right: &TreeNode{Val: 4}}}
+	sums := []int{1, 9, 17, 22}
+	results := []bool{false, true, true, false}
+
+	for i := 0; i < len(sums); i++ {
+		if r := hasPathSum(root, sums[i]); r != results[i] {
+			t.Log(sums[i], r)
+			t.Fail()
+		}
+	}
+}
