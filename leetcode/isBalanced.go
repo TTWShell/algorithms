c457928630297cf02aa09988531c9e75ee39e88e
Legolas Bloom <zhanhsw@gmail.com>
2017-05-23 12:11:43 +0800

    add isBalanced

---
+++ leetcode/isBalanced.go
@@ -0,0 +1,24 @@
+/* https://leetcode.com/problems/balanced-binary-tree/#/description
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+*/
+
+package leetcode
+
+func abs(i int) int {
+	if i >= 0 {
+		return i
+	}
+	return -i
+}
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if abs(maxDepth(root.Left)-maxDepth(root.Right)) > 1 {
+		return false
+	}
+	return isBalanced(root.Left) && isBalanced(root.Right)
+}
