6c9375077d4231ac39ab3baa6a735ffc2f4c61ee
Legolas Bloom <zhanhsw@gmail.com>
2017-05-25 07:50:19 +0800

    add twoSum2

--- leetcode/array/twoSum2.go
+++ leetcode/array/twoSum2.go
@@ -12,5 +12,16 @@ Output: index1=1, index2=2
 package leetcode
 
 func twoSum2(numbers []int, target int) []int {
-	return []int{2, 3}
+	start, end := 0, len(numbers)-1
+	for start < end {
+		sum := numbers[start] + numbers[end]
+		if sum == target {
+			return []int{start + 1, end + 1}
+		} else if sum > target {
+			end--
+		} else {
+			start++
+		}
+	}
+	return []int{}
 }
