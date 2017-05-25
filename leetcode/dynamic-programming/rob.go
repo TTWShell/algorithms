e5a7a82bb1d9dc2b91dcc9dcfa4257ae316bea48
Legolas Bloom <zhanhsw@gmail.com>
2017-05-25 21:34:59 +0800

    add thinking

--- leetcode/dynamic-programming/rob.go
+++ leetcode/dynamic-programming/rob.go
@@ -2,6 +2,9 @@
 You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security system connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
 
 Given a list of non-negative integers representing the amount of money of each house, determine the maximum amount of money you can rob tonight without alerting the police.
+
+d[i] = max(d[i-2]+nums[i], d[i-1])
+第i次要么抢，要么不抢，收益总为：第i－2次＋本次 与 i－1 的最大值。
 */
 
 package leetcode
