9bab21e72bbba62b06d939c45d4367bb54fd6074
Legolas Bloom <zhanhsw@gmail.com>
2017-05-25 11:55:48 +0800

    don't use hash table

--- leetcode/array/majorityElement.go
+++ leetcode/array/majorityElement.go
@@ -10,18 +10,31 @@ Special thanks to @ts for adding this problem and creating all test cases.
 package leetcode
 
 func majorityElement(nums []int) int {
-	maps := make(map[int]int)
-	mid := len(nums) / 2
-	for i := 0; i < len(nums); i++ {
-		if v, ok := maps[nums[i]]; ok {
-			v += 1
-			maps[nums[i]] = v
+	/*maps := make(map[int]int)
+	  mid := len(nums) / 2
+	  for i := 0; i < len(nums); i++ {
+	      if v, ok := maps[nums[i]]; ok {
+	          v += 1
+	          maps[nums[i]] = v
+	      } else {
+	          maps[nums[i]t ] = 1
+	      }
+	      if v, ok := maps[nums[i]]; ok && v > mid {
+	          return nums[i]
+	      }
+	  }
+	  panic("nums cannot be empty")
+	*/
+	cur, count := nums[0], 1
+	for i := 1; i < len(nums); i++ {
+		if count == 0 {
+			count = 1
+			cur = nums[i]
+		} else if cur == nums[i] {
+			count++
 		} else {
-			maps[nums[i]] = 1
-		}
-		if v, ok := maps[nums[i]]; ok && v > mid {
-			return nums[i]
+			count--
 		}
 	}
-	panic("nums cannot be empty")
+	return cur
 }
