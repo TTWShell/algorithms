e2cc4c116b3e332a5df45c0b61154a304f5ff318
Legolas Bloom <zhanhsw@gmail.com>
2017-05-27 12:14:38 +0800

    add containsNearbyDuplicate

--- leetcode/hash-table/containsNearbyDuplicate.go
+++ leetcode/hash-table/containsNearbyDuplicate.go
@@ -2,10 +2,23 @@
 Given an array of integers and an integer k,
 find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j]
 and the absolute difference between i and j is at most k.
+
+给出一个整数数组，判断该数组内是否有两个元素值是相同的，且他们的索引值相差不大于k，是则返回true，否则返回false
 */
 
 package leetcode
 
 func containsNearbyDuplicate(nums []int, k int) bool {
-	return true
+	maps := make(map[int]int)
+	for i := range nums {
+		if _, ok := maps[nums[i]]; !ok {
+			maps[nums[i]] = i
+		} else {
+			if i-maps[nums[i]] <= k {
+				return true
+			}
+			maps[nums[i]] = i
+		}
+	}
+	return false
 }
--- leetcode/hash-table/containsNearbyDuplicate_test.go
+++ leetcode/hash-table/containsNearbyDuplicate_test.go
@@ -3,8 +3,8 @@ package leetcode
 import "testing"
 
 func Test_containsNearbyDuplicate(t *testing.T) {
-	input := [][]int{{1, 2, 3, 1, 3}}
-	result := []bool{false}
+	input := [][]int{{-1, 0, 1, -1}, {1, 2, 3, 1, 3}}
+	result := []bool{true, true}
 
 	for i := range input {
 		if r := containsNearbyDuplicate(input[i], 3); r != result[i] {
