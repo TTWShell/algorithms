153f4b6f5b00b27f6fd6cba9c524d4d38f630d49
Legolas Bloom <zhanhsw@gmail.com>
2017-05-27 08:05:31 +0800

    init containsNearbyDuplicate

---
+++ leetcode/hash-table/containsNearbyDuplicate.go
@@ -0,0 +1,11 @@
+/* https://leetcode.com/problems/contains-duplicate-ii/#/description
+Given an array of integers and an integer k,
+find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j]
+and the absolute difference between i and j is at most k.
+*/
+
+package leetcode
+
+func containsNearbyDuplicate(nums []int, k int) bool {
+	return true
+}
