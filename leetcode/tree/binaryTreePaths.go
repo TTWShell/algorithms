5e4d944b0b53ba6ea4db25774faa789805f52458
Legolas Bloom <zhanhsw@gmail.com>
2017-05-28 15:14:23 +0800

    init binaryTreePaths

---
+++ leetcode/tree/binaryTreePaths.go
@@ -0,0 +1,20 @@
+/* https://leetcode.com/problems/binary-tree-paths/#/description
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+
+   1
+ /   \
+2     3
+ \
+  5
+All root-to-leaf paths are:
+
+["1->2->5", "1->3"]
+*/
+
+package leetcode
+
+func binaryTreePaths(root *TreeNode) []string {
+	return []string{"", ""}
+}
