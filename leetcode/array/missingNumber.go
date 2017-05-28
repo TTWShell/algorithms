0c5b4edac0b2e7f2e1c230ae446c561028b00030
Legolas Bloom <zhanhsw@gmail.com>
2017-05-28 17:09:49 +0800

    init missingNumber

---
+++ leetcode/array/missingNumber.go
@@ -0,0 +1,15 @@
+/* https://leetcode.com/problems/missing-number/#/description
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+*/
+
+package leetcode
+
+func missingNumber(nums []int) int {
+	return 0
+}
