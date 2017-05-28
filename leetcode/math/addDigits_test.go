e20c3c047b73fc7c646d179ec5a57d5d1ccd9c99
Legolas Bloom <zhanhsw@gmail.com>
2017-05-28 16:30:47 +0800

    init addDigits

---
+++ leetcode/math/addDigits_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import "testing"
+
+func Test_addDigits(t *testing.T) {
+	nums := []int{38, 22, 138}
+	results := []int{2, 4, 3}
+
+	for i := range nums {
+		if r := addDigits(nums[i]); r != results[i] {
+			t.Fatal(nums[i], r)
+		}
+	}
+}
