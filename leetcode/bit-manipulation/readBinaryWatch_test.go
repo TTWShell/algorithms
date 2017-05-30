1e68eaedb5631690c8424005a82b7255f354eeef
Legolas Bloom <zhanhsw@gmail.com>
2017-05-30 23:11:05 +0800

    init readBinaryWatch

---
+++ leetcode/bit-manipulation/readBinaryWatch_test.go
@@ -0,0 +1,24 @@
+package leetcode
+
+import (
+	"sort"
+	"testing"
+)
+
+func Test_readBinaryWatch(t *testing.T) {
+	num := 1
+	result := []string{"1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"}
+	sort.Slice(result[:], func(i, j int) bool { return result[i] < result[j] })
+
+	r := readBinaryWatch(num)
+
+	if len(r) != len(result) {
+		t.Fatal(num, r)
+	}
+
+	for i := range result {
+		if r[i] != result[i] {
+			t.Fatal(num, r)
+		}
+	}
+}
