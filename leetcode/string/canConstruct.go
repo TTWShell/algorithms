8f03f1ffbc27969a62916faa118f1a0f6da48771
Legolas Bloom <zhanhsw@gmail.com>
2017-05-30 12:37:59 +0800

    add canConstruct

--- leetcode/string/canConstruct.go
+++ leetcode/string/canConstruct.go
@@ -1,6 +1,6 @@
 /* https://leetcode.com/problems/ransom-note/#/description
 Given an arbitrary ransom note string and another string containing letters from all the magazines,
-write a function that will return true if the ransom note can be constructed from the magazines ;
+write a functioransom-noten that will return true if the ransom note can be constructed from the magazines ;
 otherwise, it will return false.
 
 Each letter in the magazine string can only be used once in your ransom note.
@@ -16,5 +16,17 @@ Note:
 package leetcode
 
 func canConstruct(ransomNote string, magazine string) bool {
+	maps := make(map[rune]int)
+	for _, r := range magazine {
+		maps[r] += 1
+	}
+
+	for _, r := range ransomNote {
+		if v, ok := maps[r]; ok && v > 0 {
+			maps[r] -= 1
+		} else {
+			return false
+		}
+	}
 	return true
 }
