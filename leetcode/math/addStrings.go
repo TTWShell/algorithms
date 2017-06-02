d156b58c8ea53573cfa48761a11c4a38ae6b266f
Legolas Bloom <zhanhsw@gmail.com>
2017-06-02 17:08:10 +0800

    add addStrings

--- leetcode/math/addStrings.go
+++ leetcode/math/addStrings.go
@@ -12,5 +12,33 @@ You must not use any built-in BigInteger library or convert the inputs to intege
 package leetcode
 
 func addStrings(num1 string, num2 string) string {
-	return ""
+	m, n := len(num1), len(num2)
+	if m < n {
+		temp := num1
+		num1, num2 = num2, temp
+		t := m
+		m, n = n, t
+	}
+
+	for i := 0; i < m-n; i++ {
+		num2 = "0" + num2
+	}
+
+	temp := make([]rune, m)
+	carry := 0
+	for i := m - 1; i >= 0; i-- {
+		sum := carry + int(num1[i]-'0') + int(num2[i]-'0')
+		if sum > 9 {
+			carry = 1
+			temp[i] = rune(sum - 10 + '0')
+		} else {
+			carry = 0
+			temp[i] = rune(sum + '0')
+		}
+	}
+
+	if carry == 1 {
+		return "1" + string(temp)
+	}
+	return string(temp)
 }
