00461a8b8da5661cf6f5b8261575a59b068a3fc6
Legolas Bloom <zhanhsw@gmail.com>
2017-06-03 17:23:09 +0800

    add findAnagrams

---
+++ leetcode/hash-table/findAnagrams_test.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "testing"
+
+func Test_findAnagrams(t *testing.T) {
+	input := [][]string{{"cbaebabacd", "abc"}, {"abab", "ab"}}
+	output := [][]int{{0, 6}, {0, 1, 2}}
+
+	for i := range input {
+		r := findAnagrams(input[i][0], input[i][1])
+		if len(r) != len(output[i]) {
+			t.Fatal(r, output[i])
+		}
+		for j := range r {
+			if r[j] != output[i][j] {
+				t.Fatal(r, output[i])
+			}
+		}
+	}
+}
