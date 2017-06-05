8247f8b75c4a92f3a10cc51d420026ddc007df0a
Legolas Bloom <zhanhsw@gmail.com>
2017-06-05 20:18:54 +0800

    add islandPerimeter

---
+++ leetcode/hash-table/islandPerimeter.go
@@ -0,0 +1,39 @@
+/* https://leetcode.com/problems/island-perimeter/#/description
+You are given a map in form of a two-dimensional integer grid where 1 represents land and 0 represents water.
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water,
+and there is exactly one island (i.e., one or more connected land cells).
+The island doesn't have "lakes" (water inside that isn't connected to the water around the island).
+One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100.
+Determine the perimeter of the island.
+
+Example:
+
+    [[0,1,0,0],
+     [1,1,1,0],
+     [0,1,0,0],
+     [1,1,0,0]]
+
+    Answer: 16
+    Explanation: The perimeter is the 16 yellow stripes in the image below:
+    https://leetcode.com/static/images/problemset/island.png
+*/
+
+package leetcode
+
+func islandPerimeter(grid [][]int) int {
+	r := 0
+	for i := range grid {
+		for j := range grid[i] {
+			if grid[i][j] == 1 {
+				r += 4
+				if i != 0 && grid[i-1][j] == 1 {
+					r -= 2
+				}
+				if j != 0 && grid[i][j-1] == 1 {
+					r -= 2
+				}
+			}
+		}
+	}
+	return r
+}
