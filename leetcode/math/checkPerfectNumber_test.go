d2c3067bb3d54e1a104a317988d8b7019fc7c435
Legolas Bloom <zhanhsw@gmail.com>
2017-06-07 21:13:23 +0800

    add checkPerfectNumber

---
+++ leetcode/math/checkPerfectNumber_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import "testing"
+
+func Test_checkPerfectNumber(t *testing.T) {
+	input := []int{1, 28}
+	output := []bool{false, true}
+
+	for i := range input {
+		if r := checkPerfectNumber(input[i]); r != output[i] {
+			t.Fatal(input[i], r)
+		}
+	}
+}
