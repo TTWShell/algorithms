b984539c85adf7306035f2c98c6cc51dc913aa51
Legolas Bloom <zhanhsw@gmail.com>
2017-06-07 12:46:24 +0800

    add findMode

--- leetcode/tree/findMode.go
+++ leetcode/tree/findMode.go
@@ -24,5 +24,28 @@ Follow up: Could you do that without using any extra space? (Assume that the imp
 package leetcode
 
 func findMode(root *TreeNode) []int {
-	return []int{}
+	var (
+		maxCount, currentVal, tempCount int
+		result                          []int
+	)
+	return inorder(root, &maxCount, &currentVal, &tempCount, result)
+}
+
+func inorder(root *TreeNode, maxCount, currentVal, tempCount *int, result []int) []int {
+	if root == nil {
+		return result
+	}
+	result = inorder(root.Left, maxCount, currentVal, tempCount, result)
+	if root.Val != *currentVal {
+		*currentVal = root.Val
+		*tempCount = 0
+	}
+	*tempCount++
+	if *tempCount > *maxCount {
+		*maxCount = *tempCount
+		result = []int{root.Val}
+	} else if *tempCount == *maxCount {
+		result = append(result, root.Val)
+	}
+	return inorder(root.Right, maxCount, currentVal, tempCount, result)
 }
