45a112e63a16559236d4925b817e8eea78cb05aa
Legolas Bloom <zhanhsw@gmail.com>
2017-06-11 20:11:09 +0800

    add mergeTrees

--- leetcode/tree/mergeTrees.go
+++ leetcode/tree/mergeTrees.go
@@ -25,5 +25,14 @@ Note: The merging process must start from the root nodes of both trees.
 package leetcode
 
 func mergeTrees(t1 *TreeNode, t2 *TreeNode) *TreeNode {
-	return t1
+	if t1 == nil && t2 == nil {
+		return nil
+	}
+	if t1 == nil && t2 != nil {
+		return t2
+	}
+	if t1 != nil && t2 == nil {
+		return t1
+	}
+	return &TreeNode{Val: t1.Val + t2.Val, Left: mergeTrees(t1.Left, t2.Left), Right: mergeTrees(t1.Right, t2.Right)}
 }
