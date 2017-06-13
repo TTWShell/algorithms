3607d42235ca019f7e2235a5cc5ff4a8103114df
Legolas Bloom <zhanhsw@gmail.com>
2017-06-13 15:18:09 +0800

    merge len(subPalindrome) even & odd process

--- leetcode/string/longestPalindrome.go
+++ leetcode/string/longestPalindrome.go
@@ -14,27 +14,17 @@ Example:
 package leetcode
 
 func longestPalindrome(s string) string {
-	helper := func(s string, start, end int) (frontIndex, endIndex int) {
-		if start < -1 || end > len(s)-1 {
-			return (start + end) / 2, (start + end) / 2
-		}
-		for start >= 0 && end < len(s) && s[start] == s[end] {
-			start--
-			end++
-		}
-		return start + 1, end - 1
-	}
-
 	rStartIndex, longLen := 0, 1
 	for index := 0; len(s)-index >= longLen/2; index++ {
-		// 按index找回文子串，以自己为中心，基数个回文串。例如s = 'aba', index = 1, return 'aba'
-		if frontIndex, endIndex := helper(s, index-1, index+1); endIndex-frontIndex+1 > longLen {
-			rStartIndex = frontIndex
-			longLen = endIndex - frontIndex + 1
-
+		frontIndex, endIndex := index, index
+		for endIndex < len(s)-1 && s[endIndex] == s[endIndex+1] {
+			endIndex++
 		}
-		// 按index找回文字串，以自己右侧为中心，偶数个。例如：s = 'abbc', index = 1, return 'bb'
-		if frontIndex, endIndex := helper(s, index, index+1); endIndex-frontIndex+1 > longLen {
+		for frontIndex > 0 && endIndex < len(s)-1 && s[frontIndex-1] == s[endIndex+1] {
+			frontIndex--
+			endIndex++
+		}
+		if endIndex-frontIndex+1 > longLen {
 			rStartIndex = frontIndex
 			longLen = endIndex - frontIndex + 1
 		}
