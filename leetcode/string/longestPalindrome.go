30b0c5619272e6ba1118b78544c272811a702c6a
Legolas Bloom <zhanhsw@gmail.com>
2017-06-13 14:52:15 +0800

    enhance code: reducing cycle times & trans to string only once

--- leetcode/string/longestPalindrome.go
+++ leetcode/string/longestPalindrome.go
@@ -14,30 +14,30 @@ Example:
 package leetcode
 
 func longestPalindrome(s string) string {
-	helper := func(s string, start, end int) string {
+	helper := func(s string, start, end int) (frontIndex, endIndex int) {
 		if start < -1 || end > len(s)-1 {
-			return string(s[(start+end)/2])
+			return (start + end) / 2, (start + end) / 2
 		}
-		for start >= 0 && end < len(s) {
-			if s[start] != s[end] {
-				break
-			}
+		for start >= 0 && end < len(s) && s[start] == s[end] {
 			start--
 			end++
 		}
-		return s[start+1 : end]
+		return start + 1, end - 1
 	}
 
-	result := ""
-	for index := 0; index < len(s); index++ {
+	rStartIndex, longLen := 0, 1
+	for index := 0; len(s)-index >= longLen/2; index++ {
 		// 按index找回文子串，以自己为中心，基数个回文串。例如s = 'aba', index = 1, return 'aba'
-		if temp := helper(s, index-1, index+1); len(temp) > len(result) {
-			result = temp
+		if frontIndex, endIndex := helper(s, index-1, index+1); endIndex-frontIndex+1 > longLen {
+			rStartIndex = frontIndex
+			longLen = endIndex - frontIndex + 1
+
 		}
 		// 按index找回文字串，以自己右侧为中心，偶数个。例如：s = 'abbc', index = 1, return 'bb'
-		if temp := helper(s, index, index+1); len(temp) > len(result) {
-			result = temp
+		if frontIndex, endIndex := helper(s, index, index+1); endIndex-frontIndex+1 > longLen {
+			rStartIndex = frontIndex
+			longLen = endIndex - frontIndex + 1
 		}
 	}
-	return result
+	return s[rStartIndex : rStartIndex+longLen]
 }
