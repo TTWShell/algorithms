f0c02c22dc3e3869da18d5988b43d685db13c979
Legolas Bloom <zhanhsw@gmail.com>
2017-06-15 17:53:42 +0800

    add convert

--- leetcode/string/convert.go
+++ leetcode/string/convert.go
@@ -16,5 +16,37 @@ convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
 package leetcode
 
 func convert(s string, numRows int) string {
-	return s
+	// 第一行和最后一行下标间隔都是interval = numRows*2-2 = 8
+	// 中间行的间隔是周期性的,第i行的间隔是: interval–2*i,  2*i,  interval–2*i, 2*i, interval–2*i, 2*i, …
+	length := len(s)
+	if numRows == 1 {
+		return s
+	}
+
+	interval := numRows<<1 - 2
+	result, index := make([]byte, len(s), len(s)), 0
+	// 处理第一行
+	for i := 0; i < length; i += interval {
+		result[index] = s[i]
+		index++
+	}
+
+	// 处理中间行
+	for i := 1; i < numRows-1; i++ {
+		inter := i << 1
+		for j := i; j < length; j += inter {
+			{
+				result[index] = s[j]
+				index++
+				inter = interval - inter
+			}
+		}
+	}
+
+	// 处理最后一行
+	for i := numRows - 1; i < length; i += interval {
+		result[index] = s[i]
+		index++
+	}
+	return string(result)
 }
