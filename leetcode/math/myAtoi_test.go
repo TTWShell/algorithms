16d9697c1f3084423c47bc4df412cd6f42b0deeb
Legolas Bloom <zhanhsw@gmail.com>
2017-06-17 20:54:30 +0800

    add myAtoi

--- leetcode/math/myAtoi_test.go
+++ leetcode/math/myAtoi_test.go
@@ -3,5 +3,12 @@ package leetcode
 import "testing"
 
 func Test_myAtoi(t *testing.T) {
+	input := []string{"  123", "  +-2", "123-+", "123dad234324", "2147483648"}
+	output := []int{123, 0, 123, 123, 2147483647}
 
+	for i := range input {
+		if r := myAtoi(input[i]); r != output[i] {
+			t.Fatal(input[i], r)
+		}
+	}
 }
