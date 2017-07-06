0bfb267efae5f0238716b10a4742873750e48249
Legolas Bloom <zhanhsw@gmail.com>
2017-07-06 15:57:56 +0800

    add intToRoman

--- leetcode/math/intToRoman.go
+++ leetcode/math/intToRoman.go
@@ -7,5 +7,11 @@ Input is guaranteed to be within the range from 1 to 3999.
 package leetcode
 
 func intToRoman(num int) string {
-	return ""
+	var (
+		M = []string{"", "M", "MM", "MMM"}
+		C = []string{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}
+		X = []string{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}
+		I = []string{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}
+	)
+	return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10]
 }
