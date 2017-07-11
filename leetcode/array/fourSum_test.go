ff42f9790872ba9419b7591db60f76bbcfa85deb
Legolas Bloom <zhanhsw@gmail.com>
2017-07-11 13:00:45 +0800

    init fourSum

---
+++ leetcode/array/fourSum_test.go
@@ -0,0 +1,22 @@
+package leetcode
+
+import "testing"
+
+func Test_fourSum(t *testing.T) {
+	input := []int{1, 0, -1, 0, -2, 2}
+	output := [][]int{
+		{-1, 0, 0, 1},
+		{-2, -1, 1, 2},
+		{-2, 0, 0, 2},
+	}
+
+	r := fourSum(input, 0)
+
+	for i, row := range output {
+		for j := range row {
+			if output[i][j] != r[i][j] {
+				t.Fatal(r)
+			}
+		}
+	}
+}
