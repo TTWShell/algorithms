981fe6b5ee4085ca434fb623cfc0cbcdf0fd66df
Legolas Bloom <zhanhsw@gmail.com>
2017-07-13 09:30:07 +0800

    add test case

--- leetcode/linked-list/removeNthFromEnd_test.go
+++ leetcode/linked-list/removeNthFromEnd_test.go
@@ -4,17 +4,26 @@ import "testing"
 
 func Test_removeNthFromEnd(t *testing.T) {
 	head := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5}}}}}
-	if r := removeNthFromEnd(head, 2); r.String() != "1 2 3 5 <nil>" {
-		t.Fatal(r)
+	if r := removeNthFromEnd(head, 2); r == nil || r.String() != "1 2 3 5 <nil>" {
+		t.Fatal("head =", head, "r =", r)
 	}
 
 	head = &ListNode{Val: 1}
 	if r := removeNthFromEnd(head, 1); r != nil {
-		t.Fatal(r)
+		t.Fatal("head =", head, "r =", r)
 	}
 
 	head = &ListNode{Val: 1, Next: &ListNode{Val: 2}}
-	if r := removeNthFromEnd(head, 2); r.String() != "2 <nil>" {
-		t.Fatal(r)
+	if r := removeNthFromEnd(head, 2); r == nil || r.String() != "2 <nil>" {
+		t.Fatal("head =", head, "r =", r)
+	}
+
+	if r := removeNthFromEnd(head, 1); r == nil || r.String() != "1 <nil>" {
+		t.Fatal("head =", head, "r =", r)
+	}
+
+	head = &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}
+	if r := removeNthFromEnd(head, 1); r == nil || r.String() != "1 2 <nil>" {
+		t.Fatal("head =", head, "r =", r)
 	}
 }
