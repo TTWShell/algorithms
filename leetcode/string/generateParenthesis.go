11480384044ceb5b091483d63da252970ff095c6
Legolas Bloom <zhanhsw@gmail.com>
2017-07-14 13:05:23 +0800

    add generateParenthesis

--- leetcode/string/generateParenthesis.go
+++ leetcode/string/generateParenthesis.go
@@ -15,5 +15,21 @@ For example, given n = 3, a solution set is:
 package leetcode
 
 func generateParenthesis(n int) []string {
-	return []string{}
+	var backtrack func(res *[]string, runes []rune, open, close, max int)
+	backtrack = func(res *[]string, runes []rune, open, close, max int) {
+		if len(runes) == max*2 {
+			*res = append(*res, string(runes))
+			return
+		}
+		if open < max {
+			backtrack(res, append(runes, '('), open+1, close, max)
+		}
+		if close < open {
+			backtrack(res, append(runes, ')'), open, close+1, max)
+		}
+	}
+
+	res := []string{}
+	backtrack(&res, []rune{}, 0, 0, n)
+	return res
 }
