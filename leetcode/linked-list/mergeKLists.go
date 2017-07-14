f500ed76500427959beb05d18d4d1d2d93e6f6e4
Legolas Bloom <zhanhsw@gmail.com>
2017-07-14 14:51:55 +0800

    add mergeKLists

--- leetcode/linked-list/mergeKLists.go
+++ leetcode/linked-list/mergeKLists.go
@@ -4,6 +4,34 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
 
 package leetcode
 
+import "math"
+
 func mergeKLists(lists []*ListNode) *ListNode {
-	return lists[0]
+	var (
+		res, tmp      *ListNode
+		minIndex, min int
+	)
+
+	for cur := res; ; lists[minIndex] = lists[minIndex].Next {
+		minIndex, min = -1, math.MaxInt32
+		for i, list := range lists {
+			if list != nil && list.Val < min {
+				min = list.Val
+				minIndex = i
+			}
+		}
+
+		if minIndex == -1 {
+			break
+		}
+
+		tmp = &ListNode{Val: min}
+		if res == nil {
+			res = tmp
+		} else {
+			cur.Next = tmp
+		}
+		cur = tmp
+	}
+	return res
 }
