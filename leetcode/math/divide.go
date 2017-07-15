3c6e95c95b417aa5c788091f4e3388b0fd6373a7
Legolas Bloom <zhanhsw@gmail.com>
2017-07-15 10:56:09 +0800

    add divide

--- leetcode/math/divide.go
+++ leetcode/math/divide.go
@@ -7,5 +7,36 @@ If it is overflow, return MAX_INT.
 package leetcode
 
 func divide(dividend int, divisor int) int {
-	return -1
+	// 参考https://discuss.leetcode.com/category/37/divide-two-integers
+	if dividend == -2147483648 && divisor == -1 {
+		return 2147483647
+	}
+
+	sign := true
+	if (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) {
+		sign = false
+	}
+
+	if dividend < 0 {
+		dividend = -dividend
+	}
+	if divisor < 0 {
+		divisor = -divisor
+	}
+
+	res := 0
+	for dividend >= divisor {
+		temp, multiple := divisor, 1
+		for (temp << 1) < dividend {
+			temp = temp << 1
+			multiple = multiple << 1
+		}
+		dividend = dividend - temp
+		res += multiple
+	}
+
+	if !sign {
+		return -res
+	}
+	return res
 }
