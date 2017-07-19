683af29036c4d917ecce265f09dd7e76c20f5157
Legolas Bloom <zhanhsw@gmail.com>
2017-07-19 15:05:52 +0800

    add nextPermutation

--- leetcode/array/nextPermutation.go
+++ leetcode/array/nextPermutation.go
@@ -14,5 +14,33 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 package leetcode
 
 func nextPermutation(nums []int) {
-	nums[1], nums[2] = nums[2], nums[1]
+	if len(nums) < 2 {
+		return
+	}
+
+	swapIndex := -1
+	for i := len(nums) - 1; i > 0; i-- {
+		if nums[i-1] < nums[i] {
+			swapIndex = i - 1
+			break
+		}
+	}
+
+	reverse := func(nums []int) {
+		for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+	}
+
+	if swapIndex != -1 {
+		for i := len(nums) - 1; i > swapIndex; i-- {
+			if nums[i] > nums[swapIndex] {
+				nums[i], nums[swapIndex] = nums[swapIndex], nums[i]
+				reverse(nums[swapIndex+1:])
+				break
+			}
+		}
+	} else {
+		reverse(nums)
+	}
 }
