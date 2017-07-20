1ed56570a1731fcf4ccf32271c6ea9d8793d2001
Legolas Bloom <zhanhsw@gmail.com>
2017-07-20 20:43:14 +0800

    add searchRange

--- leetcode/binary-search/searchRange.go
+++ leetcode/binary-search/searchRange.go
@@ -13,5 +13,23 @@ return [3, 4].
 package leetcode
 
 func searchRange(nums []int, target int) []int {
-	return []int{}
+	left, right := 0, len(nums)-1
+
+	for right >= left {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			left, right = mid, mid
+			for ; left >= 0 && nums[left] == target; left-- {
+			}
+			for ; right < len(nums) && nums[right] == target; right++ {
+			}
+			return []int{left + 1, right - 1}
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return []int{-1, -1}
 }
--- leetcode/binary-search/searchRange_test.go
+++ leetcode/binary-search/searchRange_test.go
@@ -6,12 +6,14 @@ import (
 )
 
 func Test_searchRange(t *testing.T) {
-	nums := []int{5, 7, 7, 8, 8, 10}
-
-	if r := searchRange(nums, 8); !reflect.DeepEqual(r, []int{3, 4}) {
+	if r := searchRange([]int{1}, 1); !reflect.DeepEqual(r, []int{0, 0}) {
 		t.Fatal(r)
 	}
 
+	nums := []int{5, 7, 7, 8, 8, 10}
+	if r := searchRange(nums, 8); !reflect.DeepEqual(r, []int{3, 4}) {
+		t.Fatal(r)
+	}
 	if r := searchRange(nums, 11); !reflect.DeepEqual(r, []int{-1, -1}) {
 		t.Fatal(r)
 	}
