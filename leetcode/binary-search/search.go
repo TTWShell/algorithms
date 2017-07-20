52c607e248ce2ba26784175bbb170d254ea7c99d
Legolas Bloom <zhanhsw@gmail.com>
2017-07-20 14:25:04 +0800

    use another thinking

--- leetcode/binary-search/search.go
+++ leetcode/binary-search/search.go
@@ -16,48 +16,23 @@ func search(nums []int, target int) int {
 		left, right = 0, lenn - 1
 	)
 
-	if lenn == 0 {
-		return -1
-	}
-
-	if nums[0] > nums[lenn-1] {
-		// is Rotated Sorted Array
-		if target <= nums[lenn-1] {
-			// right block, find left index
-			tmp := right
-			for tmp >= left {
-				mid := left + (tmp-left)/2
-				if nums[mid] > nums[lenn-1] {
-					left = mid + 1
-				} else {
-					tmp = mid - 1
-				}
-			}
-			left = tmp + 1
-		}
-		if target >= nums[0] {
-			// left block, find right index
-			tmp := left
-			for right >= tmp {
-				mid := tmp + (right-tmp)/2
-				if nums[mid] < nums[0] {
-					right = mid - 1
-				} else {
-					tmp = mid + 1
-				}
-			}
-			right = tmp - 1
-		}
-	}
-
 	for right >= left {
 		mid := left + (right-left)/2
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] > target {
-			right = mid - 1
+		}
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid
+			} else {
+				left = mid + 1
+			}
 		} else {
-			left = mid + 1
+			if nums[mid] < target && target <= nums[right-1] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
 		}
 	}
 
