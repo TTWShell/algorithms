b5bcd59dbe10f033c9fce9df60fcf6ceb845f6ae
Legolas Bloom <zhanhsw@gmail.com>
2017-07-25 17:28:42 +0800

    add isValidSudoku

--- leetcode/hash-table/isValidSudoku.go
+++ leetcode/hash-table/isValidSudoku.go
@@ -14,5 +14,18 @@ A valid Sudoku board (partially filled) is not necessarily solvable. Only the fi
 package leetcode
 
 func isValidSudoku(board [][]byte) bool {
-	return false
+	var rowMask, colMask, areaMask [9][9]bool
+	for r := range board {
+		for c := range board[r] {
+			if board[r][c] != '.' {
+				digit := board[r][c] - '0' - 1
+				area := 3*(r/3) + c/3
+				if rowMask[r][digit] || colMask[c][digit] || areaMask[area][digit] {
+					return false
+				}
+				rowMask[r][digit], colMask[c][digit], areaMask[area][digit] = true, true, true
+			}
+		}
+	}
+	return true
 }
--- leetcode/hash-table/isValidSudoku_test.go
+++ leetcode/hash-table/isValidSudoku_test.go
@@ -3,12 +3,30 @@ package leetcode
 import "testing"
 
 func Test_isValidSudoku(t *testing.T) {
-	board := [][]byte{}
-	input := []string{".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........"}
-	for i := range input {
-		board = append(board, []byte(input[i]))
+	initBoard := func(input []string) [][]byte {
+		board := [][]byte{}
+		for i := range input {
+			board = append(board, []byte(input[i]))
+		}
+		return board
 	}
+	board := initBoard([]string{"....5..1.", ".4.3.....", ".....3..1", "8......2.", "..2.7....", ".15......", ".....2...", ".2.9.....", "..4......"})
+	if r := isValidSudoku(board); r != false {
+		t.Fatal(r)
+	}
+
+	board = initBoard([]string{".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........"})
 	if r := isValidSudoku(board); r != true {
+		t.Fatal(r)
+	}
+
+	board = initBoard([]string{"..5.....6", "....14...", ".........", ".....92..", "5....2...", ".......3.", "...54....", "3.....42.", "...27.6.."})
+	if r := isValidSudoku(board); r != true {
+		t.Fatal(r)
+	}
+
+	board = initBoard([]string{"..4...63.", ".........", "5......9.", "...56....", "4.3.....1", "...7.....", "...5.....", ".........", "........."})
+	if r := isValidSudoku(board); r != false {
 		t.Fatal(r)
 	}
 }
