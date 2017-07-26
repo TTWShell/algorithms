f305bd031ff12475523ae0d4254faba55e71dbac
Legolas Bloom <zhanhsw@gmail.com>
2017-07-26 16:15:45 +0800

    init solveSudoku

---
+++ leetcode/hash-table/solveSudoku.go
@@ -0,0 +1,20 @@
+/* https://leetcode.com/problems/sudoku-solver/#/description
+Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+Empty cells are indicated by the character '.'.
+
+You may assume that there will be only one unique solution.
+
+http://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png
+
+A sudoku puzzle...
+
+http://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Sudoku-by-L2G-20050714_solution.svg/250px-Sudoku-by-L2G-20050714_solution.svg.png
+
+..and its solution numbers marked in red.
+*/
+
+package leetcode
+
+func solveSudoku(board [][]byte) {
+}
