20a82d5d7d85c98789d9970a5f91c57ce24e7d5b
Legolas Bloom <zhanhsw@gmail.com>
2017-08-10 10:55:16 +0800

    add combinationSum2

--- leetcode/backtracking/combinationSum2.go
+++ leetcode/backtracking/combinationSum2.go
@@ -19,6 +19,27 @@ A solution set is:
 
 package leetcode
 
+import "sort"
+
 func combinationSum2(candidates []int, target int) [][]int {
-	return [][]int{}
+	res := [][]int{}
+	sort.Ints(candidates)
+	for i := len(candidates) - 1; i >= 0; i-- {
+		if now := candidates[i]; now > target {
+			continue
+		} else if now == target {
+			res = append(res, []int{now})
+		} else {
+			results := combinationSum2(candidates[0:i], target-now)
+			if len(results) != 0 {
+				for _, result := range results {
+					res = append(res, append(result, now))
+				}
+			}
+		}
+		for i-1 >= 0 && candidates[i] == candidates[i-1] {
+			i--
+		}
+	}
+	return res
 }
--- leetcode/backtracking/combinationSum2_test.go
+++ leetcode/backtracking/combinationSum2_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test_combinationSum2(t *testing.T) {
 	if r := combinationSum2([]int{10, 1, 2, 7, 6, 1, 5}, 8); !reflect.DeepEqual(r, [][]int{
-		{1, 7}, {1, 2, 5}, {2, 6}, {1, 1, 6}}) {
+		{1, 7}, {2, 6}, {1, 1, 6}, {1, 2, 5}}) {
 		t.Fatal(r)
 	}
 }
