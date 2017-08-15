7f1413672fa9757d81f1c5bd969591b667706cef
Legolas Bloom <zhanhsw@gmail.com>
2017-08-15 16:51:33 +0800

    add trap

--- leetcode/array/trap.go
+++ leetcode/array/trap.go
@@ -10,5 +10,29 @@ http://www.leetcode.com/static/images/problemset/rainwatertrap.png
 package leetcode
 
 func trap(height []int) int {
-	return -1
+	// 2 pointers
+	var (
+		left, right       = 0, len(height) - 1
+		res               = 0
+		leftMax, rightMax = 0, 0
+	)
+
+	for left < right {
+		if height[left] < height[right] {
+			if height[left] >= leftMax {
+				leftMax = height[left]
+			} else {
+				res += leftMax - height[left]
+			}
+			left++
+		} else {
+			if height[right] >= rightMax {
+				rightMax = height[right]
+			} else {
+				res += rightMax - height[right]
+			}
+			right--
+		}
+	}
+	return res
 }
