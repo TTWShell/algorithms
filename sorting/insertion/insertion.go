970eebdbbc085b69b09cb07cafccca64481de407
Legolas Bloom <zhanhsw@gmail.com>
2017-08-21 16:28:40 +0800

    add InsertionSort

---
+++ sorting/insertion/insertion.go
@@ -0,0 +1,13 @@
+package insertion
+
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		value := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > value {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = value
+	}
+}
