77d54ea3b67e5cd75f5784cb583adfe4d810cc07
Legolas Bloom <zhanhsw@gmail.com>
2017-08-28 15:35:38 +0800

    add multiply

--- leetcode/math/multiply.go
+++ leetcode/math/multiply.go
@@ -12,5 +12,27 @@ Note:
 package leetcode
 
 func multiply(num1 string, num2 string) string {
-	return num1 + num2
+	sum := make([]byte, len(num1)+len(num2))
+
+	for i := len(num1) - 1; i >= 0; i-- {
+		var carry, bit byte
+		for j := len(num2) - 1; j >= 0; j-- {
+			bit = byte(num1[i]-'0')*byte(num2[j]-'0') + carry
+			if sum[i+j+1] != 0 {
+				bit += byte(sum[i+j+1] - '0')
+			}
+			sum[i+j+1] = bit%10 + '0'
+			carry = bit / 10
+		}
+		if carry != 0 {
+			sum[i] = carry + '0'
+		}
+	}
+
+	for i := 0; i < len(sum); i++ {
+		if sum[i] != 0 && sum[i]-'0' > 0 {
+			return string(sum[i:])
+		}
+	}
+	return "0"
 }
