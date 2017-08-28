348fc4cdd69557698d44ede9e3876b1416937984
Legolas Bloom <zhanhsw@gmail.com>
2017-08-28 14:42:03 +0800

    init multiply

---
+++ leetcode/math/multiply.go
@@ -0,0 +1,16 @@
+/* https://leetcode.com/problems/multiply-strings/description/
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+Note:
+
+    1. The length of both num1 and num2 is < 110.
+    2. Both num1 and num2 contains only digits 0-9.
+    3. Both num1 and num2 does not contain any leading zero.
+    4. You must not use any built-in BigInteger library or convert the inputs to integer directly.
+*/
+
+package leetcode
+
+func multiply(num1 string, num2 string) string {
+	return num1 + num2
+}
