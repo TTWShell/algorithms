071c19e0caf2d04a0b10d8749095b14349b87459
Legolas Bloom <zhanhsw@gmail.com>
2017-09-01 17:23:26 +0800

    add checkPossibility

---
+++ leetcode/array/checkPossibility_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import "testing"
+
+func Test_checkPossibility(t *testing.T) {
+	if r := checkPossibility([]int{4, 2, 3}); r != true {
+		t.Fatal(r)
+	}
+
+	if r := checkPossibility([]int{4, 2, 1}); r != false {
+		t.Fatal(r)
+	}
+
+	if r := checkPossibility([]int{3, 4, 2, 3}); r != false {
+		t.Fatal(r)
+	}
+}
