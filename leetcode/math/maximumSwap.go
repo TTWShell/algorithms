f9d6b6338ea27cf51457a57eb7913071b6fb6138
Legolas Bloom <zhanhsw@gmail.com>
2017-09-05 06:50:29 +0800

    add maximumSwap

--- leetcode/math/maximumSwap.go
+++ leetcode/math/maximumSwap.go
@@ -15,6 +15,42 @@ Note:
 
 package leetcode
 
+import (
+	"bytes"
+	"strconv"
+)
+
 func maximumSwap(num int) int {
-	return 0
+	nums := []int{}
+	for num > 0 {
+		nums = append(nums, num%10)
+		num /= 10
+	}
+	for i, j := 0, len(nums)-1; i <= j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	buckets := make([]int, 10, 10)
+	for i, num := range nums {
+		buckets[num] = i
+	}
+
+	nums2Int := func(nums []int) int {
+		var r bytes.Buffer
+		for _, num := range nums {
+			r.WriteRune(rune(num) + '0')
+		}
+		num, _ := strconv.Atoi(r.String())
+		return num
+	}
+
+	for i := 0; i < len(nums); i++ {
+		for j := 9; j > nums[i]; j-- {
+			if tmp := buckets[j]; tmp > i {
+				nums[i], nums[tmp] = nums[tmp], nums[i]
+				return nums2Int(nums)
+			}
+		}
+	}
+	return nums2Int(nums)
 }
