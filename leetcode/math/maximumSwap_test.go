04ac31183c18a88932e342dbb2fe1344b2832222
Legolas Bloom <zhanhsw@gmail.com>
2017-09-05 05:51:44 +0800

    init maximumSwap

---
+++ leetcode/math/maximumSwap_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import "testing"
+
+func Test_maximumSwap(t *testing.T) {
+	if r := maximumSwap(2736); r != 7236 {
+		t.Fatal(r)
+	}
+
+	if r := maximumSwap(9973); r != 9973 {
+		t.Fatal(r)
+	}
+}
