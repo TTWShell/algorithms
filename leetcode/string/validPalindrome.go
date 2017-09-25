0d5de328fd59eff149de0a754f215d421a71cad7
Legolas Bloom <zhanhsw@gmail.com>
2017-09-25 13:35:40 +0800

    add validPalindrome

---
+++ leetcode/string/validPalindrome.go
@@ -0,0 +1,36 @@
+/* https://leetcode.com/problems/valid-palindrome-ii/description/
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+    Input: "aba"
+    Output: True
+
+Example 2:
+    Input: "abca"
+    Output: True
+    Explanation: You could delete the character 'c'.
+
+Note:
+    The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+*/
+
+package leetcode
+
+func validPalindrome(s string) bool {
+	var isPalindrome func(s string, start, end, count int) bool
+	isPalindrome = func(s string, start, end, count int) bool {
+		for start < end {
+			if s[start] != s[end] {
+				if count == 0 {
+					return false
+				}
+				count--
+				return isPalindrome(s, start+1, end, count) || isPalindrome(s, start, end-1, count)
+			}
+			start++
+			end--
+		}
+		return true
+	}
+	return isPalindrome(s, 0, len(s)-1, 1)
+}
---
+++ leetcode/string/validPalindrome_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_validPalindrome(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.False(validPalindrome("abc"))
+	assert.True(validPalindrome("aba"))
+	assert.True(validPalindrome("abca"))
+	assert.False(validPalindrome("abcda"))
+	assert.True(validPalindrome("ebcbbececabbacecbbcbe"))
+}
