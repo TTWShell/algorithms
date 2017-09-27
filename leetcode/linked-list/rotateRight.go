7bdcc3806d7d806d00ccadd21f69308036cb25b6
Legolas Bloom <zhanhsw@gmail.com>
2017-09-27 19:02:36 +0800

    add rotateRight

---
+++ leetcode/linked-list/rotateRight.go
@@ -0,0 +1,38 @@
+/* https://leetcode.com/problems/rotate-list/description/
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+*/
+
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	n, cur := 1, head
+	for cur.Next != nil {
+		n++
+		cur = cur.Next
+	}
+
+	cur.Next = head
+	m := n - k%n
+	for i := 0; i < m; i++ {
+		cur = cur.Next
+	}
+	newHead := cur.Next
+	cur.Next = nil
+	return newHead
+}
