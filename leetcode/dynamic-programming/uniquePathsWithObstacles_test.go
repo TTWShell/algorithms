664b6c5f72803a0f5615cd6379aaae20848249d7
Legolas Bloom <zhanhsw@gmail.com>
2017-09-28 12:08:50 +0800

    add uniquePathsWithObstacles

---
+++ leetcode/dynamic-programming/uniquePathsWithObstacles_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_uniquePathsWithObstacles(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(2, uniquePathsWithObstacles([][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}))
+	assert.Equal(0, uniquePathsWithObstacles([][]int{{1}}))
+	assert.Equal(1, uniquePathsWithObstacles([][]int{{0}}))
+	assert.Equal(0, uniquePathsWithObstacles([][]int{{0, 1}}))
+	assert.Equal(1, uniquePathsWithObstacles([][]int{{0}, {0}}))
+}
