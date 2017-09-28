019af77af25d01eaa8383cf1347e87f143860044
Legolas Bloom <zhanhsw@gmail.com>
2017-09-28 11:17:46 +0800

    use dp

--- leetcode/dynamic-programming/uniquePaths.go
+++ leetcode/dynamic-programming/uniquePaths.go
@@ -15,14 +15,19 @@ Note: m and n will be at most 100.
 package leetcode
 
 func uniquePaths(m int, n int) int {
-	if m == 1 && n == 1 {
-		return 1
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n, n)
+		dp[i][0] = 1
+	}
+	for i := 0; i < n; i++ {
+		dp[0][i] = 1
 	}
 
-	if m == 1 {
-		return uniquePaths(1, n-1)
-	} else if n == 1 {
-		return uniquePaths(m-1, 1)
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
 	}
-	return uniquePaths(m-1, n) + uniquePaths(m, n-1)
+	return dp[m-1][n-1]
 }
