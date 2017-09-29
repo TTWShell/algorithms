f4d984dc3e8099dec76a108a286db53d73cb49ad
Legolas Bloom <zhanhsw@gmail.com>
2017-09-29 10:42:37 +0800

    constant space solution

--- leetcode/array/setZeroes_test.go
+++ leetcode/array/setZeroes_test.go
@@ -12,6 +12,22 @@ func Test_simplifyPath(t *testing.T) {
 	setZeroes(matrix)
 	assert.Equal([][]int{}, matrix)
 
+	matrix = [][]int{{0}}
+	setZeroes(matrix)
+	assert.Equal([][]int{{0}}, matrix)
+
+	matrix = [][]int{{1, 0}}
+	setZeroes(matrix)
+	assert.Equal([][]int{{0, 0}}, matrix)
+
+	matrix = [][]int{{0, 1}}
+	setZeroes(matrix)
+	assert.Equal([][]int{{0, 0}}, matrix)
+
+	matrix = [][]int{{0}, {1}}
+	setZeroes(matrix)
+	assert.Equal([][]int{{0}, {0}}, matrix)
+
 	input := [][]int{
 		{1, 0, 3},
 		{4, 5, 6},
@@ -21,6 +37,38 @@ func Test_simplifyPath(t *testing.T) {
 		{0, 0, 0},
 		{4, 0, 6},
 		{7, 0, 9},
+	}
+	setZeroes(input)
+	assert.Equal(output, input)
+
+	input = [][]int{
+		{0, 0, 0, 5},
+		{4, 3, 1, 4},
+		{0, 1, 1, 4},
+		{1, 2, 1, 3},
+		{0, 0, 1, 1},
+	}
+	output = [][]int{
+		{0, 0, 0, 0},
+		{0, 0, 0, 4},
+		{0, 0, 0, 0},
+		{0, 0, 0, 3},
+		{0, 0, 0, 0},
+	}
+	setZeroes(input)
+	assert.Equal(output, input)
+
+	input = [][]int{
+		{1, 2, 3, 4},
+		{5, 0, 5, 2},
+		{8, 9, 2, 0},
+		{5, 7, 2, 1},
+	}
+	output = [][]int{
+		{1, 0, 3, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{5, 0, 2, 0},
 	}
 	setZeroes(input)
 	assert.Equal(output, input)
