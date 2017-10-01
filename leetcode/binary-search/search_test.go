124946cd0961f46f304250386366a0488d9c518b
Legolas Bloom <zhanhsw@gmail.com>
2017-10-01 23:00:58 +0800

    enhance search test: use assert & add test case

--- leetcode/binary-search/search_test.go
+++ leetcode/binary-search/search_test.go
@@ -1,13 +1,18 @@
 package leetcode
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_search(t *testing.T) {
-	nums := []int{4, 5, 6, 7, 0, 1, 2}
+	assert := assert.New(t)
+	nums := []int{4, 5, 6, 7, -10, -5, -4, 0, 1, 2}
 
 	for i, target := range nums {
-		if r := search(nums, target); r != i {
-			t.Fatal(nums, target, r)
-		}
+		assert.Equal(i, search(nums, target))
 	}
+
+	assert.Equal(-1, search(nums, 1000))
+	assert.Equal(-1, search(nums, -1000))
 }
