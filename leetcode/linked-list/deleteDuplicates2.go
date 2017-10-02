79ae33cba6e895d7460e43823460dc19370171a6
Legolas Bloom <zhanhsw@gmail.com>
2017-10-02 11:00:44 +0800

    add deleteDuplicates2

---
+++ leetcode/linked-list/deleteDuplicates2.go
@@ -0,0 +1,30 @@
+/* https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+
+For example,
+Given 1->2->3->3->4->4->5, return 1->2->5.
+Given 1->1->1->2->3, return 2->3.
+*/
+
+package leetcode
+
+func deleteDuplicates2(head *ListNode) *ListNode {
+	var res, resCur *ListNode
+	cur, curCount := head, 1
+	for cur != nil {
+		if cur.Next == nil || cur.Val != cur.Next.Val {
+			if curCount == 1 {
+				tmp := &ListNode{Val: cur.Val}
+				if res == nil {
+					res, resCur = tmp, tmp
+				} else {
+					resCur.Next, resCur = tmp, tmp
+				}
+			}
+			curCount = 0
+		}
+		curCount++
+		cur = cur.Next
+	}
+	return res
+}
