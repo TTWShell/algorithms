27725521840ee458b66b729e201b3d6a6ceab8e4
Legolas Bloom <zhanhsw@gmail.com>
2017-10-03 22:37:25 +0800

    finished login

---
+++ analysis.py
@@ -0,0 +1,57 @@
+#! /usr/local/bin/python
+import argparse
+
+import requests
+
+
+class LeetCode:
+    LOGIN_URI = 'https://leetcode.com/accounts/login/'
+    HEADERS = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',  # NOQA E501
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
+        'Connection': 'keep-alive',
+        'Host': 'leetcode.com',
+        'Referer': 'https://leetcode.com/accounts/login/',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:56.0) Gecko/20100101 Firefox/56.0', # NOQA E501
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.session = requests.Session()
+        self.login()
+
+    def _get_csrfmiddlewaretoken(self):
+        resp = self.session.get(self.LOGIN_URI, headers=self.HEADERS)
+        # <input type='hidden' name='csrfmiddlewaretoken' value='B5yVhk7grKZU4rt3hNxGZ9NUB67lmIW0RuAg75Vx9f3WA8grGIV4qI2eFbmlK6Dq' />  # NOQA E501
+        return resp.text.split('csrfmiddlewaretoken')[1].split("'")[2]
+
+    def login(self):
+        data = {
+            'csrfmiddlewaretoken': self._get_csrfmiddlewaretoken(),
+            'login': self.username,
+            'password': self.password,
+        }
+        resp = self.session.post(
+            self.LOGIN_URI, data=data, headers=self.HEADERS)
+        if resp.status_code != 200:
+            print("Login failed.", resp.status_code)
+
+    def analysis(self):
+        pass
+
+
+parser = argparse.ArgumentParser(
+    description='Analysis leetcode problems progress.')
+parser.add_argument('-u', '--username', type=str, required=True,
+                    help='password')
+parser.add_argument('-p', '--password', type=str, required=True,
+                    help='password')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    leetcode = LeetCode(username=args.username, password=args.password)
+    leetcode.analysis()
