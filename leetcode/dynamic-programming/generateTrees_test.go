6697a5b13d473d8ae6d54aafbefb7512763931bb
Legolas Bloom <zhanhsw@gmail.com>
2017-10-07 16:17:14 +0800

    init generateTrees

---
+++ leetcode/dynamic-programming/generateTrees_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_generateTrees(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal([]*TreeNode{&TreeNode{Val: 1}}, generateTrees(1))
+
+	assert.Equal([]*TreeNode{
+		&TreeNode{Val: 1, Right: &TreeNode{Val: 2}},
+		&TreeNode{Val: 2, Left: &TreeNode{Val: 1}},
+	}, generateTrees(2))
+
+	assert.Equal([]*TreeNode{
+		&TreeNode{Val: 1, Right: &TreeNode{Val: 2, Right: &TreeNode{Val: 3}}},
+		&TreeNode{Val: 1, Right: &TreeNode{Val: 3, Left: &TreeNode{Val: 2}}},
+		&TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 3}},
+		&TreeNode{Val: 3, Left: &TreeNode{Val: 1, Right: &TreeNode{Val: 2}}},
+		&TreeNode{Val: 3, Right: &TreeNode{Val: 2, Right: &TreeNode{Val: 1}}},
+	}, generateTrees(3))
+}
