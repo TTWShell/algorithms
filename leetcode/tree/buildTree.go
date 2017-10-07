0b53cd62eb282d32d1beffeed7d94b30fbfb80be
Legolas Bloom <zhanhsw@gmail.com>
2017-10-08 01:18:07 +0800

    add buildTree

--- leetcode/tree/buildTree.go
+++ leetcode/tree/buildTree.go
@@ -8,5 +8,16 @@ You may assume that duplicates do not exist in the tree.
 package leetcode
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
-	return &TreeNode{}
+	if len(preorder) == 0 {
+		return nil
+	}
+	root := &TreeNode{Val: preorder[0]}
+	for i, val := range inorder {
+		if val == root.Val {
+			root.Left = buildTree(preorder[1:i+1], inorder[:i])
+			root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+			break
+		}
+	}
+	return root
 }
