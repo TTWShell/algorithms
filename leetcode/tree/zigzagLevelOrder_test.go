2e7d8904327c9ca2de589d7545ab6274ca68251a
Legolas Bloom <zhanhsw@gmail.com>
2017-10-08 00:40:15 +0800

    100% covered

--- leetcode/tree/zigzagLevelOrder_test.go
+++ leetcode/tree/zigzagLevelOrder_test.go
@@ -13,4 +13,6 @@ func Test_zigzagLevelOrder(t *testing.T) {
 
 	root = &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 5}}}
 	assert.Equal([][]int{{1}, {3, 2}, {4, 5}}, zigzagLevelOrder(root))
+
+	assert.Equal([][]int{}, zigzagLevelOrder(nil))
 }
