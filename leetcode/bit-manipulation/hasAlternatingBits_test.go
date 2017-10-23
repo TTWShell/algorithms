e2df43aedc894c58338968a91227748ea45bee5c
Legolas Bloom <zhanhsw@gmail.com>
2017-10-23 16:12:34 +0800

    add 693. Binary Number with Alternating Bits

---
+++ leetcode/bit-manipulation/hasAlternatingBits_test.go
@@ -0,0 +1,15 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_hasAlternatingBits(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(hasAlternatingBits(5))
+	assert.False(hasAlternatingBits(7))
+	assert.False(hasAlternatingBits(11))
+	assert.True(hasAlternatingBits(10))
+}
