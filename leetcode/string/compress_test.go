8de40a9eaa8902ff3f2fded8efea1301523aeee3
Legolas Bloom <zhanhsw@gmail.com>
2017-10-31 12:31:23 +0800

    add 443. String Compression

---
+++ leetcode/string/compress_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_compress(t *testing.T) {
+	assert := assert.New(t)
+
+	chars := []byte{'a', 'a', 'b', 'b', 'c', 'c', 'c'}
+	assert.Equal(6, compress(chars))
+	assert.Equal([]byte("a2b2c3"), chars[:6])
+
+	chars = []byte{'a'}
+	assert.Equal(1, compress(chars))
+	assert.Equal([]byte("a"), chars[:1])
+
+	chars = []byte{'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'}
+	assert.Equal(4, compress(chars))
+	assert.Equal([]byte("ab12"), chars[:4])
+
+	chars = []byte{'G', 't', 'W', 'Y', 'v', '&', ':', 'd', '#', 'k'}
+	assert.Equal(10, compress(chars))
+	assert.Equal([]byte{'G', 't', 'W', 'Y', 'v', '&', ':', 'd', '#', 'k'}, chars[:10])
+}
