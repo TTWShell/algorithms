392ff5cff25ab2dd7affbbf1df41b0e61fd8bbf2
Legolas Bloom <zhanhsw@gmail.com>
2017-11-13 22:07:50 +0800

    720. Longest Word in Dictionary

---
+++ leetcode/hash-table/longestWord_test.go
@@ -0,0 +1,16 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_longestWord(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal("world", longestWord([]string{"w", "wo", "wor", "worl", "world"}))
+	assert.Equal("apple", longestWord([]string{"a", "banana", "app", "appl", "ap", "apply", "apple"}))
+	assert.Equal("breakfast", longestWord([]string{
+		"b", "br", "bre", "brea", "break", "breakf", "breakfa", "breakfas", "breakfast",
+		"l", "lu", "lun", "lunc", "lunch", "d", "di", "din", "dinn", "dinne", "dinner"}))
+}
