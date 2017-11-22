41e07ae25cfca7a8f22bebe8af4ac2285a05dc85
Legolas Bloom <zhanhsw@gmail.com>
2017-11-22 13:25:37 +0800

    enhance test

--- graph/bfs_test.go
+++ graph/bfs_test.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -18,9 +19,25 @@ func TestBFS(t *testing.T) {
 	g.MakeEdge(node5, node3, 0)
 	g.MakeEdge(node5, node4, 0)
 
-	assert.Equal(g.BFS(node1), []*Node{node1, node2, node4, node3, node5})
-	assert.Equal(g.BFS(node2), []*Node{node2, node3, node5, node1, node4})
-	assert.Equal(g.BFS(node3), []*Node{node3, node1, node2, node4, node5})
+	res := g.BFS(node1)
+	assert.True(reflect.DeepEqual(res[0], node1) &&
+		(reflect.DeepEqual(res[1:3], []*Node{node2, node4}) || reflect.DeepEqual(res[1:3], []*Node{node4, node2})) &&
+		(reflect.DeepEqual(res[3:], []*Node{node3, node5}) || reflect.DeepEqual(res[3:], []*Node{node5, node3})))
+
+	res = g.BFS(node2)
+	assert.True(reflect.DeepEqual(res[0], node2) &&
+		(reflect.DeepEqual(res[1:3], []*Node{node3, node5}) || reflect.DeepEqual(res[1:3], []*Node{node5, node3})) &&
+		(reflect.DeepEqual(res[3:], []*Node{node1, node4}) || reflect.DeepEqual(res[3:], []*Node{node4, node1})))
+
+	res = g.BFS(node3)
+	assert.True(reflect.DeepEqual(res[0:2], []*Node{node3, node1}) &&
+		(reflect.DeepEqual(res[2:4], []*Node{node2, node4}) || reflect.DeepEqual(res[2:4], []*Node{node4, node2})) &&
+		(reflect.DeepEqual(res[4:], []*Node{node5}) || reflect.DeepEqual(res[4:], []*Node{node5})))
+
 	assert.Equal(g.BFS(node4), []*Node{node4})
-	assert.Equal(g.BFS(node5), []*Node{node5, node3, node4, node1, node2})
+
+	res = g.BFS(node5)
+	assert.True(reflect.DeepEqual(res[0], node5) &&
+		(reflect.DeepEqual(res[1:3], []*Node{node3, node4}) || reflect.DeepEqual(res[1:3], []*Node{node4, node3})) &&
+		reflect.DeepEqual(res[3:], []*Node{node1, node2}))
 }
