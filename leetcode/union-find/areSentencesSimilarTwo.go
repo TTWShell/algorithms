3d5325476077898dcf85d854422e552579016333
Legolas Bloom <zhanhsw@gmail.com>
2017-12-05 15:30:14 +0800

    fix non-local package ci error

--- leetcode/union-find/areSentencesSimilarTwo.go
+++ leetcode/union-find/areSentencesSimilarTwo.go
@@ -21,7 +21,7 @@ Note:
 
 package leetcode
 
-import "../../union-find" // "github.com/TTWShell/algorithms/union-find"
+import "github.com/TTWShell/algorithms/union-find"
 
 func areSentencesSimilarTwo(words1 []string, words2 []string, pairs [][]string) bool {
 	if len(words1) != len(words2) {
