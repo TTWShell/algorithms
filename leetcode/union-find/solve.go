276562b7a307155494ca81c3ef757560d44c658a
Legolas Bloom <zhanhsw@gmail.com>
2017-12-07 12:11:56 +0800

    add 130. Surrounded Regions

---
+++ leetcode/union-find/solve.go
@@ -0,0 +1,79 @@
+/* https://leetcode.com/problems/surrounded-regions/description/
+Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+
+For example,
+X X X X
+X O O X
+X X O X
+X O X X
+After running your function, the board should be:
+
+X X X X
+X X X X
+X X X X
+X O X X
+*/
+
+package leetcode
+
+import "github.com/TTWShell/algorithms/union-find"
+
+func solve(board [][]byte) {
+	if len(board) == 0 {
+		return
+	}
+
+	row, col := len(board), len(board[0])
+	uf := unionfind.New()
+	charO, char1 := byte('O'), byte('1')
+
+	var helper func(i, j int)
+	helper = func(i, j int) {
+		uf.Union(charO, [2]int{i, j})
+		board[i][j] = char1
+		if tmp := i - 1; tmp >= 0 && board[tmp][j] == charO {
+			helper(tmp, j)
+		}
+		if tmp := i + 1; tmp < row && board[tmp][j] == charO {
+			helper(tmp, j)
+		}
+		if tmp := j - 1; tmp >= 0 && board[i][tmp] == charO {
+			helper(i, tmp)
+		}
+		if tmp := j + 1; tmp < col && board[i][tmp] == charO {
+			helper(i, tmp)
+		}
+	}
+
+	// check around only.
+	lastRow, lastCol := row-1, col-1
+	for j := 0; j < col; j++ {
+		if board[0][j] == charO {
+			helper(0, j)
+		}
+		if board[lastRow][j] == charO {
+			helper(lastRow, j)
+		}
+	}
+	for i := 1; i < row-1; i++ {
+		if board[i][0] == charO {
+			helper(i, 0)
+		}
+		if board[i][lastCol] == charO {
+			helper(i, lastCol)
+		}
+	}
+
+	for i := range board {
+		for j := range board[i] {
+			if !uf.Connected(charO, [2]int{i, j}) {
+				board[i][j] = 'X'
+			}
+			if board[i][j] == char1 {
+				board[i][j] = charO
+			}
+		}
+	}
+}
---
+++ leetcode/union-find/solve_test.go
@@ -0,0 +1,33 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_solve(t *testing.T) {
+	assert := assert.New(t)
+
+	board := [][]byte{
+		{'X', 'X', 'X', 'X'},
+		{'X', 'O', 'O', 'X'},
+		{'X', 'X', 'O', 'X'},
+		{'X', 'O', 'X', 'X'},
+	}
+	excepted := [][]byte{
+		{'X', 'X', 'X', 'X'},
+		{'X', 'X', 'X', 'X'},
+		{'X', 'X', 'X', 'X'},
+		{'X', 'O', 'X', 'X'},
+	}
+	solve(board)
+	assert.Equal(excepted, board)
+
+	board = [][]byte{}
+	solve(board)
+	assert.Equal([][]byte{}, board)
+
+	board = [][]byte{{'O', 'O'}, {'O', 'O'}}
+	solve(board)
+	assert.Equal([][]byte{{'O', 'O'}, {'O', 'O'}}, board)
+}
