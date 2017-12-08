1f1d5b9ca18584ebda2d71d63833f8e26b69b18f
Legolas Bloom <zhanhsw@gmail.com>
2017-12-08 17:45:06 +0800

    add 143. Reorder List

---
+++ leetcode/linked-list/reorderList.go
@@ -0,0 +1,41 @@
+/* https://leetcode.com/problems/reorder-list/description/
+Given a singly linked list L: L0→L1→…→Ln-1→Ln,
+reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+You must do this in-place without altering the nodes' values.
+
+For example,
+Given {1,2,3,4}, reorder it to {1,4,2,3}.
+*/
+
+package leetcode
+
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow, fast = slow.Next, fast.Next.Next
+	}
+
+	tail, reversed, cur := slow, slow, slow.Next
+	for cur != nil {
+		cur.Next, reversed, cur = reversed, cur, cur.Next
+	}
+	tail.Next = nil
+
+	idx, cur, curHead, curReversed := 0, head, head, reversed
+	for curHead != nil && curHead != tail {
+		if idx%2 == 0 {
+			curHead = curHead.Next
+			cur.Next = curReversed
+			curReversed = curReversed.Next
+		} else {
+			cur.Next = curHead
+		}
+		cur = cur.Next
+		idx++
+	}
+}
---
+++ leetcode/linked-list/reorderList_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_reorderList(t *testing.T) {
+	assert := assert.New(t)
+
+	reorderList(nil) // test empty input can work
+
+	head := &ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}}
+	excepted := &ListNode{Val: 1, Next: &ListNode{Val: 4, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}}
+	reorderList(head)
+	assert.Equal(excepted, head)
+}
