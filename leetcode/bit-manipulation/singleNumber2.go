7372ad282f9f8bf8747dfdd642d0abbd5c6e8674
Legolas Bloom <zhanhsw@gmail.com>
2017-12-08 14:31:45 +0800

    add 137. Single Number II

---
+++ leetcode/bit-manipulation/singleNumber2.go
@@ -0,0 +1,17 @@
+/* https://leetcode.com/problems/single-number-ii/description/
+Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+*/
+
+package leetcode
+
+func singleNumber2(nums []int) int {
+	a, b := 0, 0
+	for i := 0; i < len(nums); i++ {
+		a = (a ^ nums[i]) & ^b
+		b = (b ^ nums[i]) & ^a
+	}
+	return a
+}
