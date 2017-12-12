17f965bc5d60d837cd51e54b0af7be644333bd46
Legolas Bloom <zhanhsw@gmail.com>
2017-12-12 12:17:23 +0800

    use assert

--- tree/bst_test.go
+++ tree/bst_test.go
@@ -2,7 +2,6 @@ package tree
 
 import (
 	"github.com/stretchr/testify/assert"
-	"reflect"
 	"sort"
 	"testing"
 )
@@ -10,53 +9,45 @@ import (
 func TestInsert(t *testing.T) {
 	assert := assert.New(t)
 	bst := NewBST()
-	if r := bst.Insert(1); !r || !reflect.DeepEqual(bst.root, &TreeNode{Val: 1}) {
-		t.Fatal(bst.root, r)
-	}
+
+	assert.True(bst.Insert(1))
+	assert.Equal(&TreeNode{Val: 1}, bst.root)
 
 	els := []int{3, 2, 15, 18, 23, 5, 7}
 	for _, el := range els {
-		if r := bst.Insert(el); !r {
-			t.Fatal(bst.root, r)
-		}
+		assert.True(bst.Insert(el))
 	}
 
-	if r := bst.Insert(1); r {
-		t.Fatal(bst.root, r)
-	}
+	assert.False(bst.Insert(1))
 
 	assert.Equal([]int{1, 2, 3, 5, 7, 15, 18, 23}, InOrderRecursion(bst.root))
 }
 
 func TestSearch(t *testing.T) {
+	assert := assert.New(t)
 	bst := NewBST()
-	if r := bst.Search(1); r {
-		t.Fatal(r, "empty tree should return false")
-	}
+
+	assert.False(bst.Search(1), "empty tree should return false")
 
 	els := []int{3, 2, 15, 18, 23, 5, 7}
 	for _, el := range els {
-		bst.Insert(el)
+		assert.True(bst.Insert(el))
 	}
 
 	for _, el := range els {
-		if r := bst.Search(el); !r {
-			t.Fatal(r, el)
-		}
+		assert.True(bst.Search(el))
 	}
 
-	if r := bst.Search(143414); r {
-		t.Fatal(r, "143414 should be not exist")
-	}
+	assert.False(bst.Search(143414), "143414 should be not exist")
 }
 
 func TestDelete(t *testing.T) {
+	assert := assert.New(t)
 	bst := NewBST()
-	if r := bst.Delete(1); r {
-		t.Fatal(r, "empty tree should return false")
-	}
 
-	assert := func(bst *BST, el int) {
+	assert.False(bst.Delete(1), "empty tree should return false")
+
+	assertIsBST := func(bst *BST, el int) {
 		bst.Delete(el)
 		r := InOrderRecursion(bst.root)
 		origin, sorted := []int{}, []int{}
@@ -65,9 +56,7 @@ func TestDelete(t *testing.T) {
 			sorted = append(sorted, num)
 		}
 		sort.Ints(sorted)
-		if !reflect.DeepEqual(origin, sorted) {
-			t.Fatal(r, sorted)
-		}
+		assert.Equal(sorted, origin)
 	}
 
 	els := []int{3, 2, 15, 18, 23, 5, 7, 26, 21, 1, 4, 100, 77, 87}
@@ -75,15 +64,13 @@ func TestDelete(t *testing.T) {
 		bst.Insert(el)
 	}
 	for _, el := range els {
-		assert(bst, el)
+		assertIsBST(bst, el)
 		bst.Insert(el)
 	}
 	for i := len(els) - 1; i >= 0; i-- {
-		assert(bst, els[i])
+		assertIsBST(bst, els[i])
 		bst.Insert(els[i])
 	}
 
-	if r := bst.Delete(134144); r {
-		t.Fatal(r, "not exist el")
-	}
+	assert.False(bst.Delete(134144), "not exist el")
 }
