f16dc03c78160a8e4afafb7276d1f51fd41bd6ca
Legolas Bloom <zhanhsw@gmail.com>
2017-12-12 11:22:21 +0800

    add postorderstack

--- tree/postorder.go
+++ tree/postorder.go
@@ -12,3 +12,29 @@ func PostOrderRecursion(root *TreeNode) []int {
 	res = append(res, root.Val)
 	return res
 }
+
+// PostOrderStack : 后序遍历的非递归实现。
+func PostOrderStack(root *TreeNode) []int {
+	res := []int{}
+	stack := []*TreeNode{root}
+	var prev, cur *TreeNode
+
+	for len(stack) != 0 {
+		cur = stack[len(stack)-1]
+		if (cur.Right == nil && cur.Left == nil) ||
+			(cur.Right != nil && cur.Right == prev) ||
+			(cur.Right == nil && cur.Left == prev) {
+			stack = stack[:len(stack)-1]
+			res = append(res, cur.Val)
+			prev = cur
+		} else {
+			if cur.Right != nil {
+				stack = append(stack, cur.Right)
+			}
+			if cur.Left != nil {
+				stack = append(stack, cur.Left)
+			}
+		}
+	}
+	return res
+}
--- tree/postorder_test.go
+++ tree/postorder_test.go
@@ -10,3 +10,9 @@ func TestPostOrderRecursion(t *testing.T) {
 
 	assert.Equal(ExceptedResOfPost, PostOrderRecursion(root))
 }
+
+func TestPostOrderStack(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(ExceptedResOfPost, PostOrderStack(root))
+}
