82222bcba080484bb58fc4dd58ab1ee5893dc5a5
Legolas Bloom <zhanhsw@gmail.com>
2017-12-13 15:10:45 +0800

    add 150. Evaluate Reverse Polish Notation

---
+++ leetcode/stack/evalRPN_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_evalRPN(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(9, evalRPN([]string{"2", "1", "+", "3", "*"}))
+	assert.Equal(6, evalRPN([]string{"4", "13", "5", "/", "+"}))
+	assert.Equal(3, evalRPN([]string{"4", "13", "5", "/", "+", "3", "-"}))
+}
