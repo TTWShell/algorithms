9dd517b4a84e2c70ce358ab27ca662f3346db042
Legolas Bloom <zhanhsw@gmail.com>
2017-12-26 11:21:13 +0800

    use sort.Slice

--- leetcode/sort/largestNumber.go
+++ leetcode/sort/largestNumber.go
@@ -14,32 +14,18 @@ import (
 	"strings"
 )
 
-type largestNumberString []string
-
-func (s largestNumberString) Less(i, j int) bool {
-	num1, _ := strconv.Atoi(s[i] + s[j])
-	num2, _ := strconv.Atoi(s[j] + s[i])
-	// reversed
-	return num1 > num2
-}
-
-func (s largestNumberString) Len() int {
-	return len(s)
-}
-
-func (s largestNumberString) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
 func largestNumber(nums []int) string {
 	strNums := make([]string, len(nums))
 	for i, num := range nums {
 		strNums[i] = strconv.Itoa(num)
 	}
 
-	sort.Sort(largestNumberString(strNums))
-	res := strings.Join(strNums, "")
+	sort.Slice(strNums, func(i, j int) bool {
+		// reversed
+		return strNums[i]+strNums[j] > strNums[j]+strNums[i]
+	})
 
+	res := strings.Join(strNums, "")
 	idx := 0
 	for idx < len(res)-1 && res[idx] == '0' {
 		idx++
