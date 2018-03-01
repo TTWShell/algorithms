9e9b3a2fcacb7a73e588be019997a187fc1a4408
Legolas Bloom <zhanhsw@gmail.com>
2018-03-01 10:57:44 +0800

    add 228. Summary Ranges

---
+++ leetcode/array/summaryRanges.go
@@ -0,0 +1,40 @@
+/* https://leetcode.com/problems/summary-ranges/description/
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+Example 1:
+
+	Input: [0,1,2,4,5,7]
+	Output: ["0->2","4->5","7"]
+
+Example 2:
+
+	Input: [0,2,3,4,6,8,9]
+	Output: ["0","2->4","6","8->9"]
+*/
+
+package leetcode
+
+import "strconv"
+
+func summaryRanges(nums []int) []string {
+	length := len(nums)
+	if length == 0 {
+		return []string{}
+	}
+
+	res := []string{}
+	for left, i := nums[0], 1; i <= len(nums); i++ {
+		if i == length || nums[i] != nums[i-1]+1 {
+			right := nums[i-1]
+			if left == right {
+				res = append(res, strconv.Itoa(left))
+			} else {
+				res = append(res, strconv.Itoa(left)+"->"+strconv.Itoa(right))
+			}
+			if i < length {
+				left = nums[i]
+			}
+		}
+	}
+	return res
+}
---
+++ leetcode/array/summaryRanges_test.go
@@ -0,0 +1,14 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_summaryRanges(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal([]string{}, summaryRanges([]int{}))
+	assert.Equal([]string{"0->2", "4->5", "7"}, summaryRanges([]int{0, 1, 2, 4, 5, 7}))
+	assert.Equal([]string{"0", "2->4", "6", "8->9"}, summaryRanges([]int{0, 2, 3, 4, 6, 8, 9}))
+}
