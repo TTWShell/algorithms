f833463be8625d003dad840176f7fbbb463cabac
Legolas Bloom <zhanhsw@gmail.com>
2018-03-07 15:24:15 +0800

    add 241. Different Ways to Add Parentheses

---
+++ leetcode/divide-and-conquer/diffWaysToCompute_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sort"
+	"testing"
+)
+
+func Test_diffWaysToCompute(t *testing.T) {
+	assert := assert.New(t)
+
+	res := diffWaysToCompute("2-1-1")
+	sort.Ints(res)
+	assert.Equal([]int{0, 2}, res)
+
+	res = diffWaysToCompute("1+1+1")
+	sort.Ints(res)
+	assert.Equal([]int{3, 3}, res)
+
+	res = diffWaysToCompute("2*3-4*5")
+	sort.Ints(res)
+	assert.Equal([]int{-34, -14, -10, -10, 10}, res)
+}
