e606741c8968cc784adca450105cda57ebfd6de6
Legolas Bloom <zhanhsw@gmail.com>
2018-03-08 15:51:57 +0800

    add 274. H-Index

---
+++ leetcode/hash-table/hIndex_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_hIndex(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(3, hIndex([]int{3, 0, 6, 1, 5}))
+}
