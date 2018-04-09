ba506379f0f07c35a1c7d1c7f81883178a259036
Legolas Bloom <zhanhsw@gmail.com>
2018-04-09 13:20:21 +0800

    add 804. Unique Morse Code Words

---
+++ leetcode/hash-table/uniqueMorseRepresentations_test.go
@@ -0,0 +1,12 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_uniqueMorseRepresentations(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(2, uniqueMorseRepresentations([]string{"gin", "zen", "gig", "msg"}))
+}
