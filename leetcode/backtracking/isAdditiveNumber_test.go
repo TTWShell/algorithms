83bbc1aa4a6d609f59099bc770e3cb466c705826
Legolas Bloom <zhanhsw@gmail.com>
2018-04-23 14:19:21 +0800

    add 306. Additive Number

---
+++ leetcode/backtracking/isAdditiveNumber_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_isAdditiveNumber(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(isAdditiveNumber("112358"))
+	assert.True(isAdditiveNumber("199100199"))
+	assert.False(isAdditiveNumber("1023"))
+	assert.False(isAdditiveNumber("1203"))
+	assert.True(isAdditiveNumber("000"))
+	assert.False(isAdditiveNumber("199001200"))
+}
