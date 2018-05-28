86208778a0eec2fd7a9c560c7c580f2c2080faba
Legolas Bloom <zhanhsw@gmail.com>
2018-05-28 13:21:10 +0800

    100% test coverage

--- leetcode/math/isRectangleOverlap_test.go
+++ leetcode/math/isRectangleOverlap_test.go
@@ -9,5 +9,7 @@ func Test_isRectangleOverlap(t *testing.T) {
 	assert := assert.New(t)
 
 	assert.True(isRectangleOverlap([]int{0, 0, 2, 2}, []int{1, 1, 3, 3}))
+	assert.True(isRectangleOverlap([]int{1, 1, 3, 3}, []int{0, 0, 2, 2}))
 	assert.False(isRectangleOverlap([]int{0, 0, 1, 1}, []int{1, 0, 2, 1}))
+	assert.False(isRectangleOverlap([]int{0, 0, 2, 2}, []int{1, 3, 3, 3}))
 }
