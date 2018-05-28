2f4012b3401e9c5fc97308c9fa2eb60c59d40692
Legolas Bloom <zhanhsw@gmail.com>
2018-05-28 13:17:59 +0800

    add 836. Rectangle Overlap

---
+++ leetcode/math/isRectangleOverlap_test.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_isRectangleOverlap(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(isRectangleOverlap([]int{0, 0, 2, 2}, []int{1, 1, 3, 3}))
+	assert.False(isRectangleOverlap([]int{0, 0, 1, 1}, []int{1, 0, 2, 1}))
+}
