234a74bdf8a6e663f2887e1e47a87ce8a7bb4643
Legolas Bloom <zhanhsw@gmail.com>
2018-06-13 11:00:15 +0800

    rename leetcode/greedy package

--- leetcode/greedy/canJump.go
+++ leetcode/greedy/canJump.go
@@ -11,7 +11,7 @@ A = [2,3,1,1,4], return true.
 A = [3,2,1,0,4], return false.
 */
 
-package leetcode
+package lgreedy
 
 func canJump(nums []int) bool {
 	n := len(nums)
--- leetcode/greedy/canJump_test.go
+++ leetcode/greedy/canJump_test.go
@@ -1,4 +1,4 @@
-package leetcode
+package lgreedy
 
 import (
 	"github.com/stretchr/testify/assert"
