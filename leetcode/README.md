0178e79d3bb5ef24c0bae757db3341ac7e8f8886
Legolas Bloom <zhanhsw@gmail.com>
2018-09-07 11:47:13 +0800

    add 703. Kth Largest Element in a Stream

--- leetcode/README.md
+++ leetcode/README.md
@@ -294,6 +294,7 @@
 | 696 | [Count Binary Substrings](string/countBinarySubstrings.go) | [Easy][] |
 | 697 | [Degree of an Array](array/findShortestSubArray.go) | [Easy][] |
 | 700 | [Search in a Binary Search Tree](binary-search-tree/searchBST.go) | [Easy][] |
+| 703 | [Kth Largest Element in a Stream](heap/KthLargest.go) | [Easy][] |
 | 717 | [1-bit and 2-bit Characters](array/isOneBitCharacter.go) | [Easy][] |
 | 720 | [Longest Word in Dictionary](hash-table/longestWord.go) | [Easy][] |
 | 724 | [Find Pivot Index](array/pivotIndex.go) | [Easy][] |
---
+++ leetcode/heap/KthLargest.go
@@ -0,0 +1,82 @@
+/* https://leetcode.com/problems/kth-largest-element-in-a-stream/description/
+Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream.
+For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+
+Example:
+
+    int k = 3;
+    int[] arr = [4,5,8,2];
+    KthLargest kthLargest = new KthLargest(3, arr);
+    kthLargest.add(3);   // returns 4
+    kthLargest.add(5);   // returns 5
+    kthLargest.add(10);  // returns 5
+    kthLargest.add(9);   // returns 8
+    kthLargest.add(4);   // returns 8
+
+Note:
+    You may assume that nums' length ≥ k-1 and k ≥ 1.
+*/
+
+package lheap
+
+import "sort"
+
+type KthLargest struct {
+	k    int
+	nums []int
+}
+
+func Constructor(k int, nums []int) KthLargest {
+	sort.Ints(nums)
+	return KthLargest{k: k, nums: nums}
+}
+func (this *KthLargest) Add(val int) int {
+	idx := this.findIdx(val)
+	// idx := sort.Search(len(this.nums), func(i int) bool { return this.nums[i] > val })
+	this.nums = append(this.nums, 0)
+	copy(this.nums[idx+1:], this.nums[idx:])
+	this.nums[idx] = val
+
+	return this.nums[len(this.nums)-this.k]
+}
+
+func (this *KthLargest) findIdx(val int) int {
+	// find first element > val, suppport duplicate element
+	if len(this.nums) == 0 {
+		return 0
+	}
+
+	start, end := 0, len(this.nums)-1
+	if val < this.nums[start] {
+		return start
+	}
+	if val > this.nums[end] {
+		return end + 1
+	}
+
+	for end > start {
+		mid := start + (end-start)>>1
+		if this.nums[mid] > val {
+			end = mid - 1
+		} else if this.nums[mid] == val {
+			idx := mid
+			for ; idx < len(this.nums) && this.nums[idx] == val; idx++ {
+			}
+			return idx
+		} else {
+			start = mid + 1
+		}
+	}
+
+	if start > end {
+		start, end = end, start
+	}
+	for i := start; i <= end; i++ {
+		if this.nums[i] > val {
+			return i
+		}
+	}
+	return end + 1
+}
