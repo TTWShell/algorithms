b1e270fef7a24de74f93c7dd9f4f5129562d7013
Legolas Bloom <zhanhsw@gmail.com>
2019-01-30 16:46:57 +0800

    add 976. Largest Perimeter Triangle

---
+++ leetcode/array/largestPerimeter.go
@@ -0,0 +1,49 @@
+/* https://leetcode.com/problems/largest-perimeter-triangle/
+Given an array A of positive lengths, return the largest perimeter of a triangle with non-zero area, formed from 3 of these lengths.
+
+If it is impossible to form any triangle of non-zero area, return 0.
+
+Example 1:
+
+	Input: [2,1,2]
+	Output: 5
+
+Example 2:
+
+	Input: [1,2,1]
+	Output: 0
+
+Example 3:
+
+	Input: [3,2,3,4]
+	Output: 10
+
+Example 4:
+
+	put: [3,6,2,3]
+	Output: 8
+
+Note:
+
+	3 <= A.length <= 10000
+	1 <= A[i] <= 10^6
+*/
+
+package larray
+
+import "sort"
+
+func largestPerimeter(A []int) int {
+	sorted := make([]int, len(A))
+	copy(sorted, A)
+	sort.Ints(sorted)
+
+	for i := len(A) - 1; i > 1; i-- {
+		a, b, c := sorted[i-2], sorted[i-1], sorted[i]
+		if a+b <= c {
+			continue
+		}
+		return a + b + c
+	}
+	return 0
+}
