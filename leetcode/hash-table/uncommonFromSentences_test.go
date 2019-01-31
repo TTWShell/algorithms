3b5b11e39b387868aa3f8320a4084c1d6dea33a4
Legolas Bloom <zhanhsw@gmail.com>
2019-01-31 18:00:10 +0800

    fix list order_by err

--- leetcode/hash-table/uncommonFromSentences_test.go
+++ leetcode/hash-table/uncommonFromSentences_test.go
@@ -6,6 +6,6 @@ import "github.com/stretchr/testify/assert"
 func Test_uncommonFromSentences(t *testing.T) {
 	assert := assert.New(t)
 
-	assert.Equal([]string{"sweet", "sour"}, uncommonFromSentences("this apple is sweet", "this apple is sour"))
+	assert.Subset([]string{"sweet", "sour"}, uncommonFromSentences("this apple is sweet", "this apple is sour"))
 	assert.Equal([]string{"banana"}, uncommonFromSentences("apple apple", "banana"))
 }
