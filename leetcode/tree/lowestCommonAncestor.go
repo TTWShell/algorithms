3d61b2be266f168b718f402fff27a83f5c3ebe3a
Legolas Bloom <zhanhsw@gmail.com>
2019-02-14 16:16:56 +0800

    add 235. Lowest Common Ancestor of a Binary Search Tree

--- leetcode/tree/lowestCommonAncestor.go
+++ leetcode/tree/lowestCommonAncestor.go
@@ -37,52 +37,21 @@ package ltree
  * }
  */
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if root == nil {
-		return nil
+	target1, target2 := p.Val, q.Val
+	if target1 > target2 {
+		target1, target2 = target2, target1
 	}
 
-	target := [][]int{}
-	cache := [][]*TreeNode{[]*TreeNode{root}}
-	flag := true
-	for tmp := 0; flag == true; tmp++ {
-		flag = false
-		cur := []*TreeNode{}
-		for idx, node := range cache[tmp] {
-			if node == nil {
-				cur = append(cur, nil)
-				cur = append(cur, nil)
-			} else {
-				if node.Val == p.Val || node.Val == q.Val {
-					target = append(target, []int{tmp, idx})
-					if len(target) == 2 {
-						break
-					}
-				}
-				if node.Left != nil || node.Right != nil {
-					flag = true
-				}
-				cur = append(cur, node.Left)
-				cur = append(cur, node.Right)
-			}
+	var helper func(root *TreeNode) *TreeNode
+	helper = func(root *TreeNode) *TreeNode {
+		if target1 <= root.Val && root.Val <= target2 {
+			return root
 		}
-		cache = append(cache, cur)
+		if target2 < root.Val {
+			return helper(root.Left)
+		}
+		return helper(root.Right)
 	}
 
-	x1, y1 := target[0][0], target[0][1]
-	x2, y2 := target[1][0], target[1][1]
-	for x1 != x2 || y1 != y2 {
-		if x1 != x2 {
-			if x1 > x2 {
-				x1, y1 = x1-1, y1/2
-			} else {
-				x2, y2 = x2-1, y2/2
-			}
-		} else {
-			if y1 != y2 {
-				x1, y1 = x1-1, y1/2
-				x2, y2 = x2-1, y2/2
-			}
-		}
-	}
-	return cache[x1][y1]
+	return helper(root)
 }
