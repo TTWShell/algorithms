edf6c4ecb44ba38e324838a31d6dcb1457b9d016
Legolas Bloom <zhanhsw@gmail.com>
2019-02-15 16:01:06 +0800

    add 985. Sum of Even Numbers After Queries

---
+++ leetcode/array/sumEvenAfterQueries.go
@@ -0,0 +1,62 @@
+/* https://leetcode.com/problems/sum-of-even-numbers-after-queries/description/
+We have an array A of integers, and an array queries of queries.
+
+For the i-th query val = queries[i][0], index = queries[i][1], we add val to A[index].  Then, the answer to the i-th query is the sum of the even values of A.
+
+(Here, the given index = queries[i][1] is a 0-based index, and each query permanently modifies the array A.)
+
+Return the answer to all queries.  Your answer array should have answer[i] as the answer to the i-th query.
+
+
+Example 1:
+
+	Input: A = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]]
+	Output: [8,6,2,4]
+	Explanation:
+	At the beginning, the array is [1,2,3,4].
+	After adding 1 to A[0], the array is [2,2,3,4], and the sum of even values is 2 + 2 + 4 = 8.
+	After adding -3 to A[1], the array is [2,-1,3,4], and the sum of even values is 2 + 4 = 6.
+	After adding -4 to A[0], the array is [-2,-1,3,4], and the sum of even values is -2 + 4 = 2.
+	After adding 2 to A[3], the array is [-2,-1,3,6], and the sum of even values is -2 + 6 = 4.
+
+Note:
+
+	1 <= A.length <= 10000
+	-10000 <= A[i] <= 10000
+	1 <= queries.length <= 10000
+	-10000 <= queries[i][0] <= 10000
+	0 <= queries[i][1] < A.length
+*/
+package larray
+
+func sumEvenAfterQueries(A []int, queries [][]int) []int {
+	tmp := make([]int, len(A), len(A))
+	copy(tmp, A)
+
+	sum := 0
+	for _, value := range tmp {
+		if value%2 == 0 {
+			sum += value
+		}
+	}
+
+	res := make([]int, len(A), len(A))
+	for i, query := range queries {
+		value, idx := query[0], query[1]
+		old := tmp[idx]
+		tmp[idx] += value
+		if tmp[idx]%2 == 0 {
+			if old%2 == 0 {
+				sum += value
+			} else {
+				sum += tmp[idx]
+			}
+		} else {
+			if old%2 == 0 {
+				sum = sum - old
+			}
+		}
+		res[i] = sum
+	}
+	return res
+}
