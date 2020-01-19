cf5a0826c7dc7052174f2f3a8dce78eda377cea4
Legolas Bloom <zhanhsw@gmail.com>
2020-01-19 13:23:13 +0800

    fix priority-queue && topKFrequent use pq

--- data-structure/priority-queue/pq.go
+++ data-structure/priority-queue/pq.go
@@ -1,6 +1,8 @@
 package pq
 
-import "sync"
+import (
+	"sync"
+)
 
 type Element struct {
 	Value    interface{}
@@ -14,23 +16,18 @@ func NewElement(value interface{}, priority int) (e *Element) {
 	}
 }
 
-func (e Element) LessThan(hel Element) bool {
-	return e.Priority < hel.Priority
+func (e Element) GreaterThan(hel Element) bool {
+	return e.Priority > hel.Priority
 }
 
 type PQ struct {
 	sync.RWMutex
-	heap  []Element
-	len   int
-	isMin bool
+	heap []Element
+	len  int
 }
 
-func MinPQConstructor() (pq *PQ) {
-	return &PQ{heap: make([]Element, 0), isMin: true}
-}
-
-func MaxPQConstructor() (pq *PQ) {
-	return &PQ{heap: make([]Element, 0), isMin: false}
+func PQConstructor() (pq *PQ) {
+	return &PQ{heap: make([]Element, 0)}
 }
 
 func (pq *PQ) Len() int {
@@ -57,9 +54,9 @@ func (pq *PQ) Extract() (el Element) {
 		panic("Empty pq, cannot Extract.")
 	}
 
-	el, pq.heap[0] = pq.heap[0], pq.heap[pq.len-1]
-	pq.heap = pq.heap[:pq.len-1]
 	pq.len--
+	el, pq.heap[0] = pq.heap[0], pq.heap[pq.len]
+	pq.heap = pq.heap[:pq.len]
 	pq.precolateDown(0)
 	return
 }
@@ -74,50 +71,48 @@ func (pq *PQ) Peek() (el Element) {
 	return pq.heap[0]
 }
 
-func (pq *PQ) ChangePriority(index, priority int) {
-	pq.Lock()
-	defer pq.Unlock()
+// func (pq *PQ) ChangePriority(index, priority int) {
+// 	pq.Lock()
+// 	defer pq.Unlock()
 
-	oldEl := pq.heap[index]
-	pq.heap[index].Priority = priority
-	if pq.less(Element{Priority: priority}, oldEl) {
-		pq.precolateUp(index)
-	} else {
-		pq.precolateDown(index)
-	}
-}
+// 	oldEl := pq.heap[index]
+// 	pq.heap[index].Priority = priority
+// 	if pq.less(Element{Priority: priority}, oldEl) {
+// 		pq.precolateUp(index)
+// 	} else {
+// 		pq.precolateDown(index)
+// 	}
+// }
 
 func (pq *PQ) precolateUp(index int) {
 	// 上滤，新元素在堆中上滤直到找出正确位置
-	needUp, parent := index, index>>1
-	for needUp > 0 && pq.less(pq.heap[needUp], pq.heap[parent]) {
+	needUp, parent := index, (index-1)>>1
+	for needUp > 0 && pq.heap[needUp].GreaterThan(pq.heap[parent]) {
 		pq.heap[parent], pq.heap[needUp] = pq.heap[needUp], pq.heap[parent]
-		needUp, parent = parent, parent>>1
+		// needUp, parent = parent, (parent-1)>>1
+		needUp = parent
+		parent = (parent - 1) >> 1
+		// fmt.Println(needUp, parent)
 	}
 }
 
 func (pq *PQ) precolateDown(index int) {
 	// 下滤
-	needDown, child := index, index<<1+1
+	needDown, child := index, (index<<1)+1
 	for needDown < pq.len && child < pq.len {
-		// find min(leftChild, rightChild)
-		if child+1 < pq.len && pq.less(pq.heap[child+1], pq.heap[child]) {
+		// find max(leftChild, rightChild)
+		if child+1 < pq.len && pq.heap[child+1].GreaterThan(pq.heap[child]) {
 			child++
 		}
 
-		if pq.less(pq.heap[needDown], pq.heap[child]) {
+		if pq.heap[needDown].GreaterThan(pq.heap[child]) {
 			break
 		}
 
 		pq.heap[needDown], pq.heap[child] = pq.heap[child], pq.heap[needDown]
-		needDown, child = child, needDown<<1+1
-	}
-}
-
-func (pq *PQ) less(a, b Element) bool {
-	if pq.isMin {
-		return a.LessThan(b)
-	} else {
-		return b.LessThan(a)
+		needDown = child
+		child = (needDown << 1) + 1
+		// needDown, child = child, (needDown<<1)+1
+		// fmt.Println(needDown, child)
 	}
 }
--- leetcode/array/topKFrequent.go
+++ leetcode/array/topKFrequent.go
@@ -20,29 +20,8 @@ Note:
 package larray
 
 import (
-	"container/heap"
+	pq "../../data-structure/priority-queue"
 )
-
-// An IntHeap is a min-heap of ints.
-type IntHeap [][2]int
-
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i][1] > h[j][1] }
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *IntHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.([2]int))
-}
-
-func (h *IntHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
 
 func topKFrequent(nums []int, k int) []int {
 	countMaps := map[int]int{}
@@ -51,16 +30,14 @@ func topKFrequent(nums []int, k int) []int {
 		countMaps[num]++
 	}
 
-	h := &IntHeap{}
-	heap.Init(h)
+	pQueue := pq.PQConstructor()
 	for k, v := range countMaps {
-		heap.Push(h, [2]int{k, v})
+		pQueue.Insert(pq.Element{Value: k, Priority: v})
 	}
 
 	res := make([]int, k)
-	for i := 0; i < k; i++ {
-		val := heap.Pop(h).([2]int)
-		res[i] = val[0]
+	for i := range res {
+		res[i] = pQueue.Extract().Value.(int)
 	}
 	return res
 }
